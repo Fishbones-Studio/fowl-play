e97a968d94914394ff3bbd3f83b6ed4a3f451bb5
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-03-10 22:06:24 +0100

    Fix camera movement

--- utilities/camera/follow_camera/follow_camera.gd
+++ utilities/camera/follow_camera/follow_camera.gd
@@ -1,4 +1,4 @@
-extends SpringArm3D
+extends Node3D
 
 ## A camera system that follows a target entity with configurable parameters.
 ## 
@@ -10,40 +10,61 @@ extends SpringArm3D
 
 @export_category("Camera")
 @export var camera_reference: Camera3D
-@export var camera_spring_length: float = 3.0
+@export var camera_spring_length: float = 6.0
 @export var camera_margin: float = 0.5
+@export var camera_smoothness: float = 6.0
+@export_category("Sensitity")
+@export_range(0.1, 2.0) var horizontal_sensitivity: float = 0.5
+@export_range(0.1, 2.0) var vertical_sensitivity: float = 0.5
+@export_range(-180, 0 ) var min_degrees: float = -90.0
+@export_range(0, 180) var max_degrees: float = 45.0
 @export_group("Entity")
 @export var entity_to_follow: CharacterBody3D
-@export var entity_follow_height: float = 1.7
-@export var entity_follow_distance: float = 3.5
-@export var entity_follow_horizontal_offset: float = 1.2
+@export var entity_follow_height: float = 3.0
+@export var entity_follow_distance: float = 0.0
+@export var entity_follow_horizontal_offset: float = 0.0
 
+@onready var spring_arm_3d: SpringArm3D = $SpringArm3D
 @onready var follow_camera_transformer: RemoteTransform3D = %FollowCameraTransformer
 
 
 func _ready():
 	if (!camera_reference):
 		push_error("No Camera3D set")
-
-	follow_camera_transformer.remote_path = camera_reference.get_path()
-
-	spring_length = camera_spring_length
-	margin = camera_margin
-
-
-func _process(_delta) -> void:
-	if !entity_to_follow:
-		return
-
-	# Calculate target position
-	var target_position: Vector3 = entity_to_follow.global_transform.origin
-	target_position -= entity_to_follow.global_transform.basis.z * -entity_follow_distance
-	target_position.y += entity_follow_height
-	target_position.x += entity_follow_horizontal_offset
-
-	# Apply smooth following
-	follow_camera_transformer.global_position = target_position
-
-	# apply camera y rotation to the player
-	entity_to_follow.rotation.y = follow_camera_transformer.rotation.y
 	
+	spring_arm_3d.spring_length = camera_spring_length
+	spring_arm_3d.margin = camera_margin
+	
+	follow_camera_transformer.remote_path = camera_reference.get_path()
+	follow_camera_transformer.lerp_weight = camera_smoothness
+	
+	# Change the parent of the follow camera and set it's position
+	call_deferred("reparent", entity_to_follow)
+	position = Vector3(entity_follow_horizontal_offset, entity_follow_height, entity_follow_distance)
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		# Mouse sensitivity control
+		get_parent().rotate_y(deg_to_rad(-event.relative.x) * horizontal_sensitivity)
+		rotate_x(deg_to_rad(-event.relative.y) * vertical_sensitivity)
+#
+		_apply_camera_clamp()
+
+
+func _process(delta):
+	# Calculate controller input
+	var x_axis: float = Input.get_action_strength("right_stick_right") - Input.get_action_strength("right_stick_left")
+	var y_axis: float = Input.get_action_strength("right_stick_up") - Input.get_action_strength("right_stick_down")
+
+	# Apply controller input with sensitivity
+	rotation.y += -x_axis * horizontal_sensitivity * delta
+	rotation.x += y_axis * vertical_sensitivity * delta
+
+	_apply_camera_clamp()
+
+
+func _apply_camera_clamp():
+	# Clamp the rotation to prevent flipping
+	rotation.z = 0
+	rotation.x = clamp(rotation.x, deg_to_rad(min_degrees), deg_to_rad(45))
