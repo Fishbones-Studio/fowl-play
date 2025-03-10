5bd0f48edea432a77c933b3354789c0528468c05
Tjorn <74906283+Nrojt@users.noreply.github.com>
2025-03-10 14:14:34 +0100

    feature/basic-player-movement (#8)
    * chore: update .gitignore and add SignalManager to project.godot
    
    * chore: update .gitignore to ignore .uid files
    
    * Feature: player scene
    
    * wip: camera broken, glide broken, player in the states is null but still works?
    
    * wip: camera broken, glide broken, player in the states is null but still works?
    
    * Removed submodule addons/panku_console
    
    * feat: add panku_console submodule
    
    * fix: correct player reference check and clean up code
    
    * fix: camera
    
    * feat: enhance player state transitions with additional information parameter, moved over debug ui
    
    * fix: jump after dash fixed
    
    * feat: add fall movement speed and update glide state movement speed
    
    * feat: controller support
    
    * feat: update player movement to be relative to player orientation and refactor camera handling (camera brokey atm)
    fix: dash in direction of movement now, with fallback off forwards when standing still
    
    * refactor: clean up code formatting and improve variable naming for clarity
    
    * feat: implement coyote time for jump availability in player states
    
    * chore: update .gitignore and add SignalManager to project.godot
    
    * chore: update .gitignore to ignore .uid files
    
    * Feature: player scene
    
    * wip: camera broken, glide broken, player in the states is null but still works?
    
    * wip: camera broken, glide broken, player in the states is null but still works?
    
    * Removed submodule addons/panku_console
    
    * feat: add panku_console submodule
    
    * fix: correct player reference check and clean up code
    
    * fix: camera
    
    * feat: enhance player state transitions with additional information parameter, moved over debug ui
    
    * fix: jump after dash fixed
    
    * feat: add fall movement speed and update glide state movement speed
    
    * feat: controller support
    
    * feat: update player movement to be relative to player orientation and refactor camera handling (camera brokey atm)
    fix: dash in direction of movement now, with fallback off forwards when standing still
    
    * refactor: clean up code formatting and improve variable naming for clarity
    
    * feat: implement coyote time for jump availability in player states
    
    * fix: add ignore option for panku_console submodule
    
    * fix: git addon
    documentation: follow camera
    
    * docs: add comments and notes to clarify code functionality in various scripts
    
    * fix: player direction
    
    * refactor: improve player direction handling and input processing
    
    * fix: dash direction
    
    * fix: improve dash and jump state transitions, adjust arena size

---
+++ utilities/camera/follow_camera/follow_camera.gd
@@ -0,0 +1,49 @@
+extends SpringArm3D
+
+## A camera system that follows a target entity with configurable parameters.
+## 
+## The follow camera uses a SpringArm3D to maintain a specific distance from the target,
+## while handling collision avoidance. It updates the camera position based on the target's
+## movement and can apply the camera's Y rotation to the followed entity.
+
+
+
+@export_category("Camera")
+@export var camera_reference: Camera3D
+@export var camera_spring_length: float = 3.0
+@export var camera_margin: float = 0.5
+@export_group("Entity")
+@export var entity_to_follow: CharacterBody3D
+@export var entity_follow_height: float = 1.7
+@export var entity_follow_distance: float = 3.5
+@export var entity_follow_horizontal_offset: float = 1.2
+
+@onready var follow_camera_transformer: RemoteTransform3D = %FollowCameraTransformer
+
+
+func _ready():
+	if (!camera_reference):
+		push_error("No Camera3D set")
+
+	follow_camera_transformer.remote_path = camera_reference.get_path()
+
+	spring_length = camera_spring_length
+	margin = camera_margin
+
+
+func _process(_delta) -> void:
+	if !entity_to_follow:
+		return
+
+	# Calculate target position
+	var target_position: Vector3 = entity_to_follow.global_transform.origin
+	target_position -= entity_to_follow.global_transform.basis.z * -entity_follow_distance
+	target_position.y += entity_follow_height
+	target_position.x += entity_follow_horizontal_offset
+
+	# Apply smooth following
+	follow_camera_transformer.global_position = target_position
+
+	# apply camera y rotation to the player
+	entity_to_follow.rotation.y = follow_camera_transformer.rotation.y
+	
