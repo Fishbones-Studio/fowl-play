a54eac4651b759c04ad88fc31fba26885c8bb35e
Nrojt <tjornbrederoo@gmail.com>
2025-03-10 22:32:05 +0100

    fix: moved docs, removed redundant get_parent call

--- utilities/camera/follow_camera/follow_camera.gd
+++ utilities/camera/follow_camera/follow_camera.gd
@@ -1,21 +1,21 @@
-extends Node3D
-
 ## A camera system that follows a target entity with configurable parameters.
 ## 
 ## The follow camera uses a SpringArm3D to maintain a specific distance from the target,
 ## while handling collision avoidance. It updates the camera position based on the target's
 ## movement and can apply the camera's Y rotation to the followed entity.
-
-
+extends Node3D
 
 @export_category("Camera")
 @export var camera_reference: Camera3D
 @export var camera_spring_length: float = 6.0
 @export var camera_margin: float = 0.5
 @export var camera_smoothness: float = 6.0
+
 @export_category("Sensitity")
 @export_range(0.1, 2.0) var horizontal_sensitivity: float = 0.5
+
 @export_range(0.1, 2.0) var vertical_sensitivity: float = 0.5
+
 @export_range(-180, 0 ) var min_degrees: float = -90.0
 @export_range(0, 180) var max_degrees: float = 45.0
 @export_group("Entity")
@@ -31,13 +31,13 @@ extends Node3D
 func _ready():
 	if (!camera_reference):
 		push_error("No Camera3D set")
-	
+
 	spring_arm_3d.spring_length = camera_spring_length
 	spring_arm_3d.margin = camera_margin
-	
+
 	follow_camera_transformer.remote_path = camera_reference.get_path()
 	follow_camera_transformer.lerp_weight = camera_smoothness
-	
+
 	# Change the parent of the follow camera and set it's position
 	call_deferred("reparent", entity_to_follow)
 	position = Vector3(entity_follow_horizontal_offset, entity_follow_height, entity_follow_distance)
@@ -46,9 +46,8 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		# Mouse sensitivity control
-		get_parent().rotate_y(deg_to_rad(-event.relative.x) * horizontal_sensitivity)
+		rotate_y(deg_to_rad(-event.relative.x) * horizontal_sensitivity)
 		rotate_x(deg_to_rad(-event.relative.y) * vertical_sensitivity)
-#
 		_apply_camera_clamp()
 
 
