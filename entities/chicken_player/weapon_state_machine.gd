ac775ad8271d926f3d6cb7018919dc19d8f94e4b
cenkerA08 <cenker08@live.nl>
2025-03-13 14:19:59 +0100

    added a attack feature, enemy hitting still needs to be debugged

--- entities/chicken_player/weapon_state_machine.gd
+++ entities/chicken_player/weapon_state_machine.gd
@@ -10,6 +10,8 @@ var windup_timer
 var attack_duration
 var cooldown_timer
 
+@onready var HitBox: Area3D = $"../HitArea"
+
 func _ready():
 	if not current_weapon:
 		print(current_weapon)
@@ -18,34 +20,36 @@ func _ready():
 
 	equip_weapon(current_weapon)
 
-	windup_timer = current_weapon.windup_time
-	attack_duration = current_weapon.attack_duration
-	cooldown_timer = current_weapon.cooldown_time
+	HitBox.area_entered.connect(on_area_entered) # Connect the signal
 
 func _process(delta):
 	match current_state:
 		WeaponState.IDLE:
-			pass
+			if Input.is_action_just_pressed("attack"): # Check for mouse click
+				current_state = WeaponState.WINDUP
+				windup_timer = current_weapon.windup_time #reset timer
+				
 
 		WeaponState.WINDUP:
 			windup_timer -= delta
 			if windup_timer <= 0:
 				current_state = WeaponState.ATTACKING
 				current_weapon.attack()
-				print("Attacking with", current_weapon.name)
+				attack_duration = current_weapon.attack_duration #reset timer
+				
 
 		WeaponState.ATTACKING:
 			attack_duration -= delta
 			if attack_duration <= 0:
 				current_state = WeaponState.COOLDOWN
-				cooldown_timer = current_weapon.cooldown_time
-				print("Cooldown for", current_weapon.name)
+				cooldown_timer = current_weapon.cooldown_time #reset timer
+				
 
 		WeaponState.COOLDOWN:
 			cooldown_timer -= delta
 			if cooldown_timer <= 0:
 				current_state = WeaponState.IDLE
-				print("Ready to attack again!")
+				
 
 func equip_weapon(weapon_resource: WeaponResource):
 	if current_weapon_instance:
@@ -55,10 +59,22 @@ func equip_weapon(weapon_resource: WeaponResource):
 
 	if weapon_resource.model:
 		current_weapon_instance = weapon_resource.model.instantiate()
-		add_child(current_weapon_instance) #Corrected line
+		add_child(current_weapon_instance)
 	elif weapon_resource:
 		current_weapon_instance = weapon_resource.instantiate()
 		add_child(current_weapon_instance)
 
-	print("Equipped:", current_weapon.name)
+	
 	current_state = WeaponState.IDLE
+
+func on_area_entered(area):
+	if area.is_in_group(area.get_parent()):
+		print("enemy found")
+		if current_weapon:
+			if current_weapon.damage:
+				
+				area.get_parent().take_damage(current_weapon.damage)
+			else:
+				print("Weapon damage is not set")
+		else:
+			print("Current weapon is not set")
