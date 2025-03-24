cc0ff2e43dfb3077fc24f5f7aeb4d0a0479719ea
Tjorn <74906283+Nrojt@users.noreply.github.com>
2025-03-24 11:32:01 +0100

    feature/movement_finetune_2 (#36)
    * feat: lowered movement speeds, because arena is smaller now
    
    * feat: jump movement after letting go of movemennt buttons
    
    * fix: dash cooldown no longer triggers on every exit, but instead on dash timer timeout
    
    * fix: double jump fix
    
    * feat: implement throttled signal emission to prevent spamming
    
    * fix: double jump, glide gravity

--- entities/chicken_player/states/movement_states/glide_state.gd
+++ entities/chicken_player/states/movement_states/glide_state.gd
@@ -1,12 +1,13 @@
 extends BasePlayerMovementState
 
 @export_range(0, 1, 0.01) var glide_gravity: float = 0.4
-@export var glide_fall_speed: float = 1.5
 @export var glide_movement_speed: float = 25
 @export var glide_stamina_cost: int = 35
 
 
 func enter(_previous_state: PlayerEnums.PlayerStates, _information: Dictionary = {}) -> void:
+	# set the y velocity to 0
+	#player.velocity.y = 0
 
 	# check for stamina
 	if player.stamina < glide_stamina_cost:
@@ -22,6 +23,11 @@ func process(_delta: float) -> void:
 	# subtract stamina
 	player.stamina -= glide_stamina_cost * _delta
 
+	# if stamina is less than 0, go to fall state
+	if player.stamina <= (glide_stamina_cost *_delta):
+		print("Not enough stamina to glide")
+		SignalManager.player_transition_state.emit(PlayerEnums.PlayerStates.FALL_STATE, {})
+
 	# check if the player is holding the jump button
 	if Input.is_action_just_released("jump"):
 		SignalManager.player_transition_state.emit(PlayerEnums.PlayerStates.FALL_STATE, {})
@@ -35,10 +41,11 @@ func physics_process(delta: float) -> void:
 	var gravity: float = player.get_gravity().y * glide_gravity
 	player.velocity.y += gravity * delta
 
-	# Clamp vertical speed
-	player.velocity.y = min(player.velocity.y, glide_fall_speed)
-
 	# Check for state transitions
 	if player.is_on_floor():
 		SignalManager.player_transition_state.emit(PlayerEnums.PlayerStates.IDLE_STATE, {})
-		
+
+
+func exit() -> void:
+	# reset the gravity scale
+	pass
