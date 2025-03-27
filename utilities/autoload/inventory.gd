c76be0769905ee5c9891f0b737d9503efbfdb968
NickFrietman <21055378@student.hhs.nl>
2025-03-27 17:50:41 +0100

    comments

--- utilities/autoload/inventory.gd
+++ utilities/autoload/inventory.gd
@@ -44,26 +44,29 @@ func get_item_by_type(item_type: ItemEnums.ItemTypes) -> Array[Resource]:
 func remove_item(item: Resource) -> void:
 	items_in_inventory = items_in_inventory.filter(func(i): return i != item)
 	save_inventory()
-	
+
+#Function to save inventory
 func save_inventory() -> void:
 	var save_data: Dictionary = {"items": []}
 	
+	#Place iventory items into save_data
 	for item in items_in_inventory:
 		if item.resource_path != "":
 			save_data["items"].append(item.resource_path)
 		else:
 			print("WARNING: Item has no resource path!", item)
-		
+	#Write save_data to a file
 	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
 	file.store_string(JSON.stringify(save_data))
 	file.close()
 	print("Inventory Saved! ", save_data)
 	
 func load_inventory() -> void:
+	# Check if file exists
 	if not FileAccess.file_exists(SAVE_FILE_PATH):
 		print("No save file found. ")
 		return
-		
+	#Store all items into save_data
 	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
 	var save_data = JSON.parse_string(file.get_as_text())
 	file.close()
@@ -73,7 +76,7 @@ func load_inventory() -> void:
 		return
 		
 	items_in_inventory.clear()
-	
+	#Loop save_data for all items and add them to the inventory
 	for item_path in save_data["items"]:
 		if ResourceLoader.exists(item_path):
 			var item = load(item_path) as Resource
