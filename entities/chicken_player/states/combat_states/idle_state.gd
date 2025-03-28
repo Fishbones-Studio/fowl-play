1e6be038d90fa778c5aeb5a232b91013090bc0d5
b9b0s8 <snives@hotmail.nl>
2025-03-28 13:25:12 +0100

    removed setup function in IdleState

--- entities/chicken_player/states/combat_states/idle_state.gd
+++ entities/chicken_player/states/combat_states/idle_state.gd
@@ -6,10 +6,6 @@ extends BaseCombatState
 # Defines this state as IDLE
 const STATE_TYPE: int = WeaponEnums.MeleeState.IDLE
 
-func setup(weapon_node: Node3D) -> void:
-	weapon = weapon_node
-
-
 # Checks for player input (attack button press)
 func input(event: InputEvent) -> void:
 	if event.is_action_pressed("attack"):
