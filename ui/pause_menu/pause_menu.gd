b14ef741d178d16c702143a01809d929ded85c89
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-03-28 02:27:16 +0100

    Add button transitions

---
+++ ui/pause_menu/pause_menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+var footer: PanelContainer
+
+
+func setup(params: Dictionary = {}) -> void:
+	init()
+	load_footer()
+	setup_button_transitions()
+
+
+# TODO: Make this modular
+func load_footer():
+	# Position footer below the screen
+	footer.position.y += footer.size.y
+	
+	# Create tween to slide footer up from bottom of the screen
+	var tween = create_tween()
+	tween.tween_property(footer, "position:y", 592, 0.3).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+
+
+func scale(node: Control, scale_xy: Vector2, duration: float = 0.2, 
+	transition: Tween.TransitionType = Tween.TRANS_CUBIC, 
+	ease: Tween.EaseType = Tween.EASE_OUT) -> void:
+	var tween = node.create_tween()
+	tween.tween_property(node, "scale", scale_xy, duration).set_trans(transition).set_ease(ease)
+
+
+func setup_button_transitions():
+	var buttons = find_children("", "Button", true)  # Find all Button nodes
+	
+	for button in buttons:
+		print("Found button:", button.name)
+	
+	for button in buttons:
+		button.mouse_entered.connect(func(): scale(button, Vector2(1.1, 1.1)))
+		button.mouse_exited.connect(func(): scale(button, Vector2(1.0, 1.0)))
+
+
+func init() -> void:
+	footer = %Footer
