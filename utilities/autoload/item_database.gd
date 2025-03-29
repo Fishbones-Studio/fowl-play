251ddaad6c8cec86640587167af972d44d027bad
Nrojt <tjornbrederoo@gmail.com>
2025-03-29 12:55:47 +0100

    todo

--- utilities/autoload/item_database.gd
+++ utilities/autoload/item_database.gd
@@ -2,7 +2,5 @@ extends BaseDatabase
 
 
 func _load_resources() -> void:
-	_load_items("res://resources/melee_weapons/")
-	_load_items("res://resources/ranged_weapons/")
-	_load_items("res://resources/passives/")
-	_load_items("res://resources/abilities/")
+	# TODO: fix the inventory system. Needs rewrite
+	pass
