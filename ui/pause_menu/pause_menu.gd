2cefb8caa3ee44aefa8418d827a7ac0f3112c0b5
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-03-30 18:46:32 +0200

    Implement tween manager functionality

--- ui/pause_menu/pause_menu.gd
+++ ui/pause_menu/pause_menu.gd
@@ -1,46 +1,45 @@
 extends Control
 
-var footer: PanelContainer
-var game_logo_container: MarginContainer
+@export var chicken: Node3D
+@export var camera: Camera3D
+@export var footer: PanelContainer
+@export var game_logo_container: MarginContainer
 
 
 func setup(params: Dictionary = {}) -> void:
-	init()
-	load_footer()
-	setup_entered_and_exit_transitions()
+	_init_footer()
+	_init_chicken()
+	_setup_enter_and_exit_transitions()
 
 
-# TODO: Make this modular
-func load_footer():
-	# Position footer below the screen
-	footer.position.y += footer.size.y
+func _init_footer():
+	footer.position.y += footer.size.y # Position footer below the screen
 	
-	# Create tween to slide footer up from bottom of the screen
-	var tween = create_tween()
-	tween.tween_property(footer, "position:y", 592, 0.3).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	TweenManager.create_move_tween(null, footer, "y", 592)
 
 
-func scale(node: Control, scale_xy: Vector2, duration: float = 0.2, 
-	transition: Tween.TransitionType = Tween.TRANS_CUBIC, 
-	ease: Tween.EaseType = Tween.EASE_OUT) -> void:
-	var tween = node.create_tween()
-	tween.tween_property(node, "scale", scale_xy, duration).set_trans(transition).set_ease(ease)
-
-
-func setup_entered_and_exit_transitions():
-	var buttons = find_children("", "Button", true)  # Find all Button nodes
+func _init_chicken():
+	var chicken_tween: Tween = chicken.create_tween()
 	
-	for button in buttons:
-		print("Found button:", button.name)
+	TweenManager.create_scale_tween(chicken_tween, chicken, Vector3(0.7, 1.5, 0.7), 0.3)
+	TweenManager.create_scale_tween(chicken_tween, chicken, Vector3(1.1, 1.1, 1.1), 0.1)
+	TweenManager.create_scale_tween(chicken_tween, chicken, Vector3(0.9, 0.9, 0.9), 0.1)
+	TweenManager.create_scale_tween(chicken_tween, chicken, Vector3(1.0, 1.0, 1.0), 0.1)
+	
+	camera.position.y = 5.5
+	TweenManager.create_move_tween(null, camera, "y", 1.5, 0.3)
+
+
+func _setup_enter_and_exit_transitions():
+	var buttons = find_children("", "Button", true)
 	
 	for button in buttons:
-		button.mouse_entered.connect(func(): scale(button, Vector2(1.1, 1.1)))
-		button.mouse_exited.connect(func(): scale(button, Vector2(1.0, 1.0)))
+		button.mouse_entered.connect(func(): 
+			TweenManager.create_scale_tween(null, button, Vector2(1.1, 1.1)))
+		button.mouse_exited.connect(func():
+			TweenManager.create_scale_tween(null, button, Vector2(1.0, 1.0)))
 	
-	game_logo_container.mouse_entered.connect(func(): scale(game_logo_container, Vector2(1.1, 1.1)))
-	game_logo_container.mouse_exited.connect(func(): scale(game_logo_container, Vector2(1.0, 1.0)))
-
-
-func init() -> void:
-	footer = %Footer
-	game_logo_container = %GameLogoContainer
+	game_logo_container.mouse_entered.connect(func():
+		TweenManager.create_scale_tween(null, game_logo_container, Vector2(1.1, 1.1)))
+	game_logo_container.mouse_exited.connect(func(): 
+		TweenManager.create_scale_tween(null, game_logo_container, Vector2(1.0, 1.0)))
---
+++ utilities/autoload/tween_manager.gd
@@ -0,0 +1,64 @@
+extends Node
+
+const DEFAULT_DURATION: float = 0.2
+const DEFAULT_TRANSITION: Tween.TransitionType = Tween.TRANS_CUBIC
+const DEFAULT_EASE: Tween.EaseType = Tween.EASE_OUT
+
+
+func chain(tweens: Array[Tween]) -> void:
+	assert(tweens.size() > 1, "At least two tweens must be provided")
+	
+	for i in range(tweens.size() - 1):
+		tweens[i].finished.connect(func(): tweens[i+1].play)
+	
+	if tweens.size() > 0:
+		tweens[0].play()
+
+
+func parallel(tweens: Array[Tween]) -> void:
+	assert(tweens.size() > 1, "At least two tweens must be provided")
+	
+	for t in tweens:
+		t.play()
+
+
+func create_scale_tween(
+	tween: Tween,
+	node: Node,
+	scale: Variant,
+	duration: float = DEFAULT_DURATION,
+	transition: Tween.TransitionType = DEFAULT_TRANSITION,
+	ease: Tween.EaseType = DEFAULT_EASE
+) -> Tween:
+	assert(scale is Vector2 or scale is Vector3,
+			"Scale must be a Vector2 or Vector3")
+	
+	if not tween: tween = node.create_tween()
+	tween.tween_property(node, "scale", scale, duration)\
+			.set_trans(transition)\
+			.set_ease(ease)
+	
+	return tween
+
+
+func create_move_tween(
+	tween: Tween,
+	node: Node,
+	axis: String,
+	distance: float,
+	duration: float = DEFAULT_DURATION,
+	transition: Tween.TransitionType = DEFAULT_TRANSITION,
+	ease: Tween.EaseType = DEFAULT_EASE
+) -> Tween:
+	assert(node != null, "Target node cannot be null")
+	assert(axis.to_lower() in ["x", "y", "z"], 
+		   "Axis must be 'x', 'y', or 'z'")
+	
+	var property := "position:%s" % axis.to_lower()
+	
+	if not tween: tween = node.create_tween()
+	tween.tween_property(node, property, distance, duration)\
+		.set_trans(transition)\
+		.set_ease(ease)
+	
+	return tween
