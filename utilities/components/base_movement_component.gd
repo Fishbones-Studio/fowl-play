1961973272af849eccc2d65920c69d24df48a252
Bastiaan Smeets <22068228@student.hhs.nl>
2025-04-03 13:07:59 +0200

    Changed gravity calculation formula to now be an acceleration? my mat… (#82)
    * Add pause menu ui
    
    * Rename nodes
    
    * Add button transitions
    
    * Add transition for game logo
    
    * Add chicken in pause menu
    
    * Implement tween manager functionality
    
    * Increase bg blur, fix create_move_tween
    
    * Add contentitems for setting menu
    
    * Add settings menu, move some stuff, use different color palette
    
    * Add pause menu functionality
    
    * Add settings menu
    
    * Add settings menu accessible via pause, and quit functionality from pause menu
    
    Some stuff is a bit whack
    
    * Remove item_database.db autoload
    
    * Add dynamic settings header
    
    * Update tween_manager.gd
    
    * Update keybind_menu.gd
    
    * idk
    
    * Fix shop
    
    * pause menu now overlaps shop
    
    * FIx overlapping somewhat
    
    * Update pause_menu.gd
    
    * Fix main menu uid error
    
    * Add collison layers and mask
    
    * Adjust movement calculation
    
    * Update base_movement_component.gd
    
    * Add forfeit button
    
    * Fix in arena shop collision layers and masks
    
    Dunno why uid changed, still doesn't work though. Resourceloader cant read path
    
    * Fix in arena shop
    
    * Add togglable sidebar items for better feedback
    
    moving with tab still doesn't work as expected
    
    * Fix tab navigation toggle
    
    Sth weird is going with keybinding sidebar idk
    
    * Update settings_menu.gd
    
    Sidebar items are very jittery but not very noticeable
    
    * Hide footer from pause menu, and removed tooltips
    
    * Changed gravity calculation formula to now be an acceleration? my math somehow worked don't quote me on this. Sidenote, don't make weight 1.
    
    ---------
    
    Co-authored-by: Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
    Co-authored-by: Junyi Xie <yunyi.xie@outlook.com>

--- utilities/components/base_movement_component.gd
+++ utilities/components/base_movement_component.gd
@@ -7,8 +7,8 @@ class_name BaseMovementComponent
 extends Node
 
 # Ratio defaults (adjust these to tweak feel)
-const DEFAULT_PEAK_RATIO: float = 0.12  # seconds per meter of jump height
-const DEFAULT_FALL_RATIO: float = 0.10   # seconds per meter of jump height
+const DEFAULT_PEAK_RATIO: float = 30 # seconds per meter of jump height
+const DEFAULT_FALL_RATIO: float = 36 # seconds per meter of jump height
 
 @export var entity: Node3D
 
@@ -34,21 +34,22 @@ func _ready():
 func _recalculate_jump_params():
 	# Calculate times if not manually specified
 	if time_to_peak <= 0:
-		time_to_peak = jump_height * DEFAULT_PEAK_RATIO
+		time_to_peak = jump_height / DEFAULT_PEAK_RATIO
 	if time_to_ground <= 0:
-		time_to_ground = jump_height * DEFAULT_FALL_RATIO
+		time_to_ground = jump_height / DEFAULT_FALL_RATIO
 
 	# Calculate physics values
-	jump_velocity = (2.0 * jump_height) / time_to_peak
-	jump_gravity = (-2.0 * jump_height) / (time_to_peak * time_to_peak)
-	fall_gravity = (-2.0 * jump_height) / (time_to_ground * time_to_ground)
+	jump_velocity = (1.5 * jump_height) / time_to_peak
+	jump_gravity = (-jump_height) / (time_to_peak * time_to_peak)
+	fall_gravity = (-jump_height) / (time_to_ground * time_to_ground)
 
 
 ## Update the vertical motion physics based on weight
 func update_physics_by_weight(weight: int):
 	# Calculate weight multiplier using logarithmic scaling,
 	# we use 3 (kg) as standard weight. So 3 (kg) would give a multiplier of 1.0
-	# NOTE: tweek/experiment with formula and weight factor.
+	# NOTE: tweek/experiment with formula and weight factor.\
+	# NOTE: Do not set weight to 1 or below
 	var weight_multiplier: float = 1.0 + (log(weight / 3.0) / log(3.0))
 
 	# Adjust physics parameters based on weight
