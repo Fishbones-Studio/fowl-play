76f5f62260228093b11e88508c52904fd56fafe1
Junyi Xie <yunyi.xie@outlook.com>
2025-04-03 21:19:27 +0200

    yes

--- level/arena/props/hazards/knock_hazard.gd
+++ level/arena/props/hazards/knock_hazard.gd
@@ -1,8 +1,9 @@
 class_name KnockHazard
 extends BaseHazard
 
-@export var knockback_strength: float = 20.0
-@export var vertical_knockback: float = 12.0
+@export var knockback_strength: float = 220.0
+@export var vertical_knockback: float = 20.0
+@export var hazard_object: Node3D
 
 var _knockback_force: Vector3
 
@@ -13,14 +14,14 @@ func _process(_delta: float) -> void:
 
 func _on_hazard_area_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
-		var knockback_direction: Vector3 = body.transform.basis.z
+		var knockback_direction: Vector3 = hazard_object.global_position.direction_to(body.global_position)
 
 		_knockback_force = Vector3(
 			knockback_direction.x + knockback_strength,
 			vertical_knockback,
 			knockback_direction.z + knockback_strength
 		)
-
-		# Apply initial knockback force
+		
 		body.velocity = _knockback_force
+		body.move_and_slide()
 		super._on_hazard_area_body_entered(body)
