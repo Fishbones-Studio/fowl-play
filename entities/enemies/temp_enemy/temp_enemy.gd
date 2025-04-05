0d7145668964d779cb194b33e67c5ddb12b20d05
Junyi Xie <yunyi.xie@outlook.com>
2025-04-06 00:35:43 +0200

    yes

--- entities/enemies/temp_enemy/temp_enemy.gd
+++ entities/enemies/temp_enemy/temp_enemy.gd
@@ -5,7 +5,10 @@ extends CharacterBody3D
 # TODO: enemy should also use livingentity stat
 
 ## Exported Variables
-@export var health: int = 100
+@export var health: int = 1000
+
+@onready var damage_label: Marker3D = $Marker3D
+
 
 func _ready() -> void:
 	SignalManager.weapon_hit_target.connect(_take_damage)
@@ -21,7 +24,20 @@ func _take_damage(target: PhysicsBody3D, damage: int) -> void:
 		print("Enemy hit!")
 		health -= damage
 		print("Enemy took %d damage! Remaining health: %d" % [damage, health])
+		
+		var damage_popup: Node3D = preload("uid://b6cnb1t5cixqj").instantiate()
+		get_parent().add_child(damage_popup)
 
+		## Random spawn location for damage number
+		var spawn_position: Vector3 = damage_label.global_position + Vector3(
+			randf_range(-damage_popup.horizontal_spread, damage_popup.horizontal_spread),
+			randf_range(0, damage_popup.vertical_variance),
+			randf_range(-damage_popup.depth_offset, damage_popup.depth_offset)
+		)
+
+		damage_popup.global_position = spawn_position
+		damage_popup.display_damage(damage)
+		
 	if health <= 0:
 		die()
 
