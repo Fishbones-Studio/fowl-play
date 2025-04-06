e2d6f6ca715470f5eba06edf1512feff88a6c210
CenkerA08 <cenker08@live.nl>
2025-04-06 18:23:56 +0200

    added a healthbar to enemy but still not the correct way of doing it, also fixed issue when you hit woodpecker it crashes

--- entities/enemies/temp_enemy/temp_enemy.gd
+++ entities/enemies/temp_enemy/temp_enemy.gd
@@ -2,15 +2,21 @@
 class_name Enemy
 extends CharacterBody3D
 
-# TODO: enemy should also use livingentity stat
-
-## Exported Variables
-@export var health: int = 100
+@export var max_health: int = 100
+var health: int
 
 @onready var damage_label: Marker3D = $Marker3D
+@onready var health_bar_sprite: Sprite3D = $Sprite3D
+@onready var health_bar_viewport: SubViewport = $SubViewport
+@onready var health_bar_progress: ProgressBar = $SubViewport/ProgressBar
 
 
 func _ready() -> void:
+	health = max_health
+	if health_bar_progress == null:
+		printerr("Error: health_bar_progress is null! Check your node paths.")
+	else:
+		_update_health_bar()
 	SignalManager.weapon_hit_target.connect(_take_damage)
 
 
@@ -18,17 +24,24 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 
+## Private Methods
+func _update_health_bar() -> void:
+	if is_instance_valid(health_bar_progress):
+		health_bar_progress.max_value = max_health
+		health_bar_progress.value = health
+	else:
+		print("Error: health_bar_progress is not valid!")
+
+
 ## Public Methods
 func _take_damage(target: PhysicsBody3D, damage: int) -> void:
 	if target == self:
-		print("Enemy hit!")
 		health -= damage
-		print("Enemy took %d damage! Remaining health: %d" % [damage, health])
-		
+		_update_health_bar()
+
 		var damage_popup: Node3D = preload("uid://b6cnb1t5cixqj").instantiate()
 		get_parent().add_child(damage_popup)
 
-		## Random spawn location for damage number
 		var spawn_position: Vector3 = damage_label.global_position + Vector3(
 			randf_range(-damage_popup.horizontal_spread, damage_popup.horizontal_spread),
 			randf_range(0, damage_popup.vertical_variance),
@@ -37,7 +50,7 @@ func _take_damage(target: PhysicsBody3D, damage: int) -> void:
 
 		damage_popup.global_position = spawn_position
 		damage_popup.display_damage(damage)
-		
+
 	if health <= 0:
 		die()
 
