c233391e41502b750654ef4ef59f8d3e8843ad0a
b9b0s8 <snives@hotmail.nl>
2025-04-08 18:22:58 +0200

    removed commented code, added type that is returned by unclear function.

--- entities/enemies/temp_enemy/enemy_states/chase_state.gd
+++ entities/enemies/temp_enemy/enemy_states/chase_state.gd
@@ -17,15 +17,10 @@ func physics_process(delta: float) -> void:
 		SignalManager.enemy_transition_state.emit(EnemyEnums.EnemyStates.IDLE_STATE, {})
 
 
-#func _on_attack_area_body_entered(body: PhysicsBody3D) -> void:
-#	# TODO this only triggers once, if you stay in the body, the enemy will stop atacking after 1 time
-#	if body == player:
-#		SignalManager.enemy_transition_state.emit(EnemyEnums.EnemyStates.ATTACK_STATE, {})
-
 func _rotate_toward_direction(direction: Vector3, delta: float) -> void:
 	var target_angle: float = atan2(-direction.x, -direction.z) # Calculate the angle to the target direction
 	var current_angle: float = enemy.rotation.y # Get the current angle of the enemy
 
 	# Lerp the angle to smoothly rotate towards the target direction
-	var new_angle := lerp_angle(current_angle, target_angle, rotation_speed * delta)
+	var new_angle : float = lerp_angle(current_angle, target_angle, rotation_speed * delta)
 	enemy.rotation.y = new_angle
