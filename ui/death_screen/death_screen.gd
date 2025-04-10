69790e32f9b339e545e6eef364df1a01a8357618
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-04-10 15:30:20 +0200

    Fix/death screen (#105)
    * nuked inputblocker and renamed game over to death screen menu
    
    * damage number bigger

--- ui/death_screen/death_screen.gd
+++ ui/death_screen/death_screen.gd
@@ -18,8 +18,7 @@ func _input(event: InputEvent) -> void:
 		and not animation_player.is_playing()
 		and not is_transitioning
 	):
-
-			_return_to_game_menu()
+		_return_to_game_menu()
 
 
 func _return_to_game_menu() -> void:
@@ -30,6 +29,10 @@ func _return_to_game_menu() -> void:
 
 	SignalManager.switch_game_scene.emit("uid://21r458rvciqo")
 
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
 	await animation_player.animation_finished
 
 	SignalManager.switch_ui_scene.emit("uid://dnq3em8w064n4")
+	
+	get_tree().paused = false
