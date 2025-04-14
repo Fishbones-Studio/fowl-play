4bb26bde449548e0f21e88bb8f00ac832674b7ee
b9b0s8 <snives@hotmail.nl>
2025-04-14 12:30:20 +0200

    added early returns to not crash the game when no weapon is equiped.

--- entities/chicken_player/weapons/handlers/player_weapon_controller.gd
+++ entities/chicken_player/weapons/handlers/player_weapon_controller.gd
@@ -5,24 +5,19 @@ var current_weapon_index := 0
 var active_weapon: Node3D
 
 func _ready() -> void:
-	if get_children().size() == 0:
-		# TODO: some default weapon
-		push_error("No weapons")
-		queue_free()
-		return
-	_activate_weapon(current_weapon_index)
+	_swap_weapoon(current_weapon_index)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("switch_weapon"):
 		current_weapon_index = (current_weapon_index + 1) % get_child_count()
-		_activate_weapon(current_weapon_index)
+		_swap_weapoon(current_weapon_index)
 	
 	if event.is_action_pressed("attack"):
 		_start_attack()
 	elif event.is_action_released("attack"):
 		_stop_attack()
 
-func _activate_weapon(index: int) -> void:
+func _swap_weapoon(index: int) -> void:
 	# Deactivate current weapon
 	if active_weapon:
 		active_weapon.visible = false
@@ -35,12 +30,14 @@ func _activate_weapon(index: int) -> void:
 	print("Switched to weapon: ", active_weapon.name)
 
 func _start_attack() -> void:
-	if active_weapon && active_weapon is RangedWeaponPlayerSlot:
+	if not active_weapon: return
+	if active_weapon is RangedWeaponPlayerSlot:
 		active_weapon.ranged_weapon_player_controller.start_firing()
 	elif active_weapon is MeleeWeaponPlayerSlot:
 		active_weapon.melee_weapon_node.melee_state_machine\
 			.melee_combat_transition_state.emit(WeaponEnums.WeaponState.WINDUP, {})
 
 func _stop_attack() -> void:
-	if active_weapon && active_weapon is RangedWeaponPlayerSlot:
+	if not active_weapon: return
+	if active_weapon is RangedWeaponPlayerSlot:
 		active_weapon.ranged_weapon_player_controller.stop_firing()
