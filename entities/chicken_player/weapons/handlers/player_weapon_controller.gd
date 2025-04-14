67fd92a8c972267022ddd2bbb0f52c24c59faf7e
Nrojt <tjornbrederoo@gmail.com>
2025-04-14 12:39:35 +0200

    fix: fixed errors when no weapons were in the inventory

--- entities/chicken_player/weapons/handlers/player_weapon_controller.gd
+++ entities/chicken_player/weapons/handlers/player_weapon_controller.gd
@@ -5,11 +5,11 @@ var current_weapon_index := 0
 var active_weapon: Node3D
 
 func _ready() -> void:
+	print("Amount of weapons: %d"  % get_child_count())
 	_swap_weapoon(current_weapon_index)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("switch_weapon"):
-		current_weapon_index = (current_weapon_index + 1) % get_child_count()
 		_swap_weapoon(current_weapon_index)
 	
 	if event.is_action_pressed("attack"):
@@ -17,17 +17,26 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_released("attack"):
 		_stop_attack()
 
-func _swap_weapoon(index: int) -> void:
-	# Deactivate current weapon
-	if active_weapon:
-		active_weapon.visible = false
-		if active_weapon is RangedWeaponPlayerSlot:
-			active_weapon.ranged_weapon_player_controller.stop_firing()
+func _swap_weapoon(prev_index: int) -> void:
+	var children : int = get_child_count()
 	
-	# Activate new weapon
-	active_weapon = get_children()[index]
-	active_weapon.visible = true
-	print("Switched to weapon: ", active_weapon.name)
+	if children <= 0:
+		push_warning("No weapon children")
+		queue_free()
+		return
+	elif children > 1:
+		current_weapon_index = (current_weapon_index + 1) % children
+		
+		# Deactivate current weapon
+		if active_weapon:
+			active_weapon.visible = false
+			if active_weapon is RangedWeaponPlayerSlot:
+				active_weapon.ranged_weapon_player_controller.stop_firing()
+		
+		# Activate new weapon
+		active_weapon = get_children()[current_weapon_index]
+		active_weapon.visible = true
+		print("Switched to weapon: ", active_weapon.name)
 
 func _start_attack() -> void:
 	if not active_weapon: return
