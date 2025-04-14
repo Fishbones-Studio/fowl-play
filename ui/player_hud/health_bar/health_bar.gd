c5ae9c59dfa07c659cdbb46d211f1b6f8f598d14
CenkerA08 <cenker08@live.nl>
2025-04-14 23:39:13 +0200

    fixed heal shader on start up

--- ui/player_hud/health_bar/health_bar.gd
+++ ui/player_hud/health_bar/health_bar.gd
@@ -41,32 +41,33 @@ func _update_health_color() -> void:
 	add_theme_stylebox_override("fill", style_box)
 
 
-# Rest of the functions remain the same...
 func set_health(_health: float) -> void:
 	if _health == health:
+		return
+	elif health <= 0:
+		health = _health
 		return
 	
 	var prev_health: float = health
 	health = _health
-	value = _health
 	
-	_update_health_color()
+	value = _health
 
 	if health <= prev_health:
 		timer.start()
-		if not tracked_entity:
-			SignalManager.player_hurt.emit()
+		SignalManager.player_hurt.emit()
 	else:
 		if health > prev_health:
 			timer.stop()
-			if not tracked_entity:
-				SignalManager.player_heal.emit()
+			SignalManager.player_heal.emit()
 		_on_timer_timeout()
 
 
+
 func init_health(_max_health: int, _health: int) -> void:
-	max_value = _max_health
+	print("init_health")
 	health = _health
+	max_value = _max_health
 	value = health
 	damage_bar.max_value = max_value
 	damage_bar.value = value
