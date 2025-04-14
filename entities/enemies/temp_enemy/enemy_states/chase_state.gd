20f301e72fa699c86770dbd43c7c99f763489bd7
b9b0s8 <snives@hotmail.nl>
2025-04-14 11:07:13 +0200

    Storing changes, may not be able to merge directly. Changed enemy movement to be more of a force rather than just walking towards player directly

--- entities/enemies/temp_enemy/enemy_states/chase_state.gd
+++ entities/enemies/temp_enemy/enemy_states/chase_state.gd
@@ -1,16 +1,17 @@
 extends BaseEnemyState
-@export var rotation_speed: float = 5.0   ## How quickly enemy turns toward target
+
 var target_position: Vector3
 
+var calculated_speed: float
 
 #Check what conditions are fulfilled to shift the enemy in state to certain behaviour patterns.
 #This would be the place to change behaviour, for example a ranged attack.
 func physics_process(delta: float) -> void:
 	target_position = (player.position - enemy.position).normalized()
 	if enemy.position.distance_to(player.position) < chase_distance:
-		if target_position.length() > 0:
-			_rotate_toward_direction(target_position, delta)
-		apply_movement(target_position * enemy.stats.calculate_speed(movement_component.walk_speed_factor))
+		calculated_speed = enemy.stats.calculate_speed(movement_component.walk_speed_factor)
+		_rotate_toward_direction(target_position, delta)
+		apply_movement(target_position * calculated_speed)
 		apply_gravity(delta)
 	else:
 		SignalManager.enemy_transition_state.emit(EnemyEnums.EnemyStates.IDLE_STATE, {})
@@ -21,5 +22,5 @@ func _rotate_toward_direction(direction: Vector3, delta: float) -> void:
 	var current_angle: float = enemy.rotation.y # Get the current angle of the enemy
 
 	# Lerp the angle to smoothly rotate towards the target direction
-	var new_angle : float = lerp_angle(current_angle, target_angle, rotation_speed * delta)
+	var new_angle : float = lerp_angle(current_angle, target_angle, calculated_speed * delta)
 	enemy.rotation.y = new_angle
