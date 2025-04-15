276bd539bb957897b6bf0fed9bdffea03aa491b0
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-04-15 22:29:41 +0200

    using roundenums to determine the state of the round

--- level/round/round_handler.gd
+++ level/round/round_handler.gd
@@ -6,8 +6,7 @@ extends Node
 @export_category("Round Settings")
 @export var max_rounds: int = 3
 @export var round_intermission: bool = true
-@export var battle_duration: float = 0 # Leave at 0 for no time limit
-@export var intermission_duration: float # The duration for the intermissions
+@export var round_timer: bool = false # If enabled, match lasts 120 seconds
 
 var round_state: RoundEnums.RoundTypes = RoundEnums.RoundTypes.WAITING
 var available_enemies: Dictionary = {}
@@ -21,12 +20,6 @@ var _current_enemy: Enemy # The one currently in the arena fighting
 
 
 func _ready() -> void:
-	battle_timer.wait_time = battle_duration
-	intermission_timer.wait_time = intermission_duration if round_intermission else 30.0
-
-	battle_timer.timeout.connect(_on_battle_timer_timeout)
-	intermission_timer.timeout.connect(_on_intermission_timer_timeout)
-
 	_init_enemies()
 	_start_round()
 
@@ -46,17 +39,61 @@ func _init_enemies() -> void:
 
 
 func _start_round() -> void:
+	match round_state:
+		RoundEnums.RoundTypes.WAITING:
+			_enter_waiting()
+		RoundEnums.RoundTypes.IN_PROGRESS:
+			_enter_in_progress()
+		RoundEnums.RoundTypes.CONCLUDING:
+			_enter_concluding()
+		RoundEnums.RoundTypes.INTERMISSION:
+			_enter_intermission()
+		_:
+			assert(false, "Invalid RoundState: %s" % str(round_state))
+
+
+func _enter_waiting() -> void:
+	print("in waiting")
+	GameManager.chicken_player.global_position = player_position.global_position
+
+	round_state = RoundEnums.RoundTypes.IN_PROGRESS
+
+	await get_tree().create_timer(5.0).timeout
+
+	_start_round()
+
+
+func _enter_in_progress() -> void:
+	print("in progress")
 	if _current_enemy == null:
-		print(available_enemies)
-		
 		_current_enemy = _pick_random_enemy(
-			EnemyEnums.EnemyTypes.BOSS 
+			EnemyEnums.EnemyTypes.BOSS
 				if GameManager.current_round == max_rounds 
 				else EnemyEnums.EnemyTypes.REGULAR
 		)
 
-	battle_timer.start()
 	_spawn_enemy_in_level()
+
+	await SignalManager.enemy_died
+	round_state = RoundEnums.RoundTypes.CONCLUDING
+	_start_round()
+
+
+func _enter_concluding() -> void:
+	print("in concluding")
+	round_state = RoundEnums.RoundTypes.INTERMISSION
+	
+	await get_tree().create_timer(5.0).timeout
+
+	_start_round()
+	
+
+func _enter_intermission() -> void:
+	print("in intermission")
+	intermission_timer.start()
+
+	# Player should teleport to some area where they can interact with shop and heal
+	print("imagine you are now in a different part of arena")
 
 
 func _pick_random_enemy(enemy_type: EnemyEnums.EnemyTypes) -> Enemy:
@@ -72,14 +109,13 @@ func _spawn_enemy_in_level() -> void:
 
 	SignalManager.enemy_died.connect(func():
 		_current_enemy = null
-		_start_round()
 		, CONNECT_ONE_SHOT
 	)
 
 
 # This function probably won't be used, but who knows, maybe for tie breakers
 # Compares both the ChickenPlayer and Enemy health and whoever has the lowest remaining health loses
-func _on_battle_timer_timeout() -> void:
+func _on_round_battle_timer_timeout() -> void:
 	if GameManager.chicken_player.stats.current_health > _current_enemy.stats.current_health:
 		print("you have more hp and procceed to next round")
 		# go to next round
@@ -88,7 +124,7 @@ func _on_battle_timer_timeout() -> void:
 		# show game over screen
 
 
-func _on_intermission_timer_timeout() -> void:
-	pass
-	
+func _on_round_intermission_timer_timeout() -> void:
+	round_state = RoundEnums.RoundTypes.WAITING # Reset back to waiting
+
 	_start_round()
