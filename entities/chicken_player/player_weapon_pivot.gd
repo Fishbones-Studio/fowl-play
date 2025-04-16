e4a2e5908338bf7170977a903a36886a7c0d623b
CenkerA08 <cenker08@live.nl>
2025-04-16 19:07:19 +0200

    the weapon now has a pivot, but looks a bit weird when you look down

---
+++ entities/chicken_player/player_weapon_pivot.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+func _process(delta):
+	var viewport = get_viewport()
+	var viewport_size = viewport.get_visible_rect().size
+	var screen_center = viewport_size / 2.0
+	var camera = viewport.get_camera_3d()
+
+	if camera:
+		# Shoot a ray straight forward from screen center
+		# Multiply by 30 to pick a point far away
+		var target_position = camera.global_position + camera.project_ray_normal(screen_center) * 30.0
+		look_at(target_position)
+
+		# Get the current vertical angle relative to the world up vector
+		var current_vertical_angle = camera.global_transform.basis.get_euler().x
+	else:
+		printerr("Error: No 3D camera found in the viewport!")
