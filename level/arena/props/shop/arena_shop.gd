e7c0bac7e1db3f33283ecb3fbe03567cf2f6a79f
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-04-16 22:27:39 +0200

    added the arena shop back

--- level/arena/props/shop/arena_shop.gd
+++ level/arena/props/shop/arena_shop.gd
@@ -5,25 +5,14 @@ var player_in_area: bool = false
 @onready var label = $Area3D/OpenShopLabel
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	label.visible = false
-
-
-func _input(event) -> void:
-	if event.is_action_pressed("interact") and player_in_area:
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_pressed("interact") and player_in_area:
 		open_shop()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
 
 
 func open_shop() -> void:
 	SignalManager.add_ui_scene.emit("uid://djg6luy3rxi23")
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	get_tree().paused = true
 
 
 func _on_area_3d_body_entered(_body: ChickenPlayer) -> void:
--- ui/in_arena_shop/shop/in_arena_shop.gd
+++ ui/in_arena_shop/shop/in_arena_shop.gd
@@ -1,7 +1,8 @@
 class_name InArenaShop
 extends BaseShop
 
-const IN_ARENA_SHOP_ITEM_SCENE = preload("uid://b1xvduw1f032y")
+const IN_ARENA_SHOP_ITEM_SCENE: PackedScene = preload("uid://b1xvduw1f032y")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,11 +15,11 @@ func _ready() -> void:
 
 func close_ui() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	get_tree().paused = false
 	queue_free() 
 
+
 func create_shop_item(selected_item : BaseResource) -> BaseShopItem:
-	var shop_item_instance = IN_ARENA_SHOP_ITEM_SCENE.instantiate()
+	var shop_item_instance: Control = IN_ARENA_SHOP_ITEM_SCENE.instantiate()
 
 	if not shop_item_instance:
 		push_error("Failed to instantiate Shop Item scene!")
@@ -29,7 +30,7 @@ func create_shop_item(selected_item : BaseResource) -> BaseShopItem:
 		push_error("Instantiated shop item node does not have the expected script type.")
 		shop_item_instance.queue_free()
 		return null
-		
+
 	shop_item_script_node.set_item_data(selected_item)
-		
+
 	return shop_item_script_node
