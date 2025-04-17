5a4ba0d97df84b54bf2db2504089052f36e9c2cb
Nrojt <tjornbrederoo@gmail.com>
2025-04-17 21:28:22 +0200

    refactor: improve type annotations and add TODO for viewport signal connection

--- entities/chicken_player/player_weapon_pivot.gd
+++ entities/chicken_player/player_weapon_pivot.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
+# TODO: Once settings have been implemented, connect a signal when viewport size changes
+
 @export var camera: Camera3D = null
 
-var screen_center = Vector2.ZERO
-var last_viewport_size = Vector2.ZERO
+var screen_center: Vector2 = Vector2.ZERO
+var last_viewport_size: Vector2 = Vector2.ZERO
 
 func _ready():
 	# Grab the active 3D camera from the viewport
@@ -15,20 +17,16 @@ func _ready():
 
 func update_screen_center():
 	# Get current viewport size and set the screen center 
-	var viewport_size = get_viewport().get_visible_rect().size
+	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
 	screen_center = viewport_size / 2.1
 	last_viewport_size = viewport_size
 
-func _process(delta):
-	# If the window size changed, update the screen center
-	var viewport_size = get_viewport().get_visible_rect().size
-	if viewport_size != last_viewport_size:
-		update_screen_center()
+func _process(_delta) -> void:
 
 	if not camera:
 		return
 
 	# Figure out where the center of the screen is pointing
-	var target_position = camera.global_position + camera.project_ray_normal(screen_center) * 30.0
+	var target_position: Vector3 = camera.global_position + camera.project_ray_normal(screen_center) * 30.0
 	# Rotate this node to look at that point
 	look_at(target_position)
