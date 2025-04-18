36dbc7cdf64f445186258ee2144e51ddd6902de3
Bastiaan Smeets <22068228@student.hhs.nl>
2025-04-18 14:18:09 +0200

    Feature/enemy dash attack (#137)
    * dash state for enemy
    
    * Dash state/attack
    
    * fix dash state for enemy
    
    * moved combat states to weapon, gave enemy fox dash and updated nodes
    
    ---------
    
    Co-authored-by: Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
    Co-authored-by: Sly <114733626+MrMisterMisterMister@users.noreply.github.com>

--- entities/weapons/combat_states/base_combat_state.gd
+++ entities/weapons/combat_states/base_combat_state.gd
@@ -2,10 +2,12 @@ class_name BaseCombatState
 extends BaseState
 
 @export var ANIMATION_NAME: String
-var melee_combat_transition_state : Signal
+
+var melee_combat_transition_state: Signal
 
 var weapon_node: MeleeWeapon
-var entity_stats : LivingEntityStats
+var entity_stats: LivingEntityStats
+
 
 func setup(_weapon_node: MeleeWeapon, _melee_combat_transition_state: Signal, _entity_stats : LivingEntityStats) -> void:
 	if not _weapon_node:
@@ -14,6 +16,7 @@ func setup(_weapon_node: MeleeWeapon, _melee_combat_transition_state: Signal, _e
 	weapon_node = _weapon_node
 	melee_combat_transition_state = _melee_combat_transition_state
 	entity_stats = _entity_stats
+
 
 func enter(_previous_state, _information: Dictionary = {}) -> void:
 	pass
