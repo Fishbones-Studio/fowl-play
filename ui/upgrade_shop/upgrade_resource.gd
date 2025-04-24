23cb717051fb08bb8b201a306571315913f37288
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-04-24 16:52:30 +0200

    changed upgraderesource to just flat bonus, easier to work with

--- ui/upgrade_shop/upgrade_resource.gd
+++ ui/upgrade_shop/upgrade_resource.gd
@@ -1,12 +1,12 @@
 class_name UpgradeResource
 extends BaseResource
 
-@export var health_bonus: float = 0.0
-@export var stamina_bonus: float = 0.0
+@export var health_bonus: int = 0
+@export var stamina_bonus: int = 0
 @export var attack_multiplier_bonus: float = 0.0
 @export var defense_bonus: int = 0
-@export var speed_bonus: float = 0.0
-@export var weight_bonus: float = 0.0
+@export var speed_bonus: int = 0
+@export var weight_bonus: int = 0
 
 
 func _init() -> void:
@@ -16,17 +16,17 @@ func _init() -> void:
 func get_modifier_string(hex_code: String = "#ffff00") -> Array[String]:
 	var modifiers: Array[String] = []
 
-	if health_bonus != 0.0:
-		modifiers.append("[color=%s]%+.2f[/color]" % [hex_code, health_bonus])
-	if stamina_bonus != 0.0:
-		modifiers.append("[color=%s]%+.2f[/color]" % [hex_code, stamina_bonus])
+	if health_bonus != 0:
+		modifiers.append("[color=%s]%+d[/color]" % [hex_code, health_bonus])
+	if stamina_bonus != 0:
+		modifiers.append("[color=%s]%+d[/color]" % [hex_code, stamina_bonus])
 	if attack_multiplier_bonus != 0.0:
-		modifiers.append("[color=%s]%+.2f[/color]" % [hex_code, attack_multiplier_bonus])
+		modifiers.append("[color=%s]%+d[/color]" % [hex_code, attack_multiplier_bonus])
 	if defense_bonus != 0:
 		modifiers.append("[color=%s]%+d[/color]" % [hex_code, defense_bonus])
-	if speed_bonus != 0.0:
-		modifiers.append("[color=%s]%+.2f[/color]" % [hex_code, speed_bonus])
-	if weight_bonus != 0.0:
-		modifiers.append("[color=%s]%+.2f[/color]" % [hex_code, weight_bonus])
+	if speed_bonus != 0:
+		modifiers.append("[color=%s]%+d[/color]" % [hex_code, speed_bonus])
+	if weight_bonus != 0:
+		modifiers.append("[color=%s]%+d[/color]" % [hex_code, weight_bonus])
 
 	return modifiers
