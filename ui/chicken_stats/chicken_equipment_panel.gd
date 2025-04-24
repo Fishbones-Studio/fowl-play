4b06bdd84cfc1f8e02cef979345987bbb03ccdcd
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-04-24 13:36:01 +0200

    Switch to dark mode

--- ui/chicken_stats/chicken_equipment_panel.gd
+++ ui/chicken_stats/chicken_equipment_panel.gd
@@ -5,9 +5,9 @@ var active: bool = false:
 	set(value):
 		active = value
 		if value:
-			add_theme_stylebox_override("panel", active_stylebox)
+			add_theme_stylebox_override("panel", active_stylebox_dark)
 		else:
-			add_theme_stylebox_override("panel", inactive_stylebox)
+			add_theme_stylebox_override("panel", inactive_stylebox_dark)
 
 var equipped_item: BaseResource
 
@@ -15,5 +15,7 @@ var equipped_item: BaseResource
 @onready var img: TextureRect = $VBoxContainer/TextureRect
 @onready var label: Label = $VBoxContainer/Label
 
-@onready var active_stylebox: StyleBoxFlat = preload("uid://bko4hnwdd8w0f")
-@onready var inactive_stylebox: StyleBoxFlat = preload("uid://b1c8gk4cr5oa7")
+#@onready var active_stylebox: StyleBoxFlat = preload("uid://bko4hnwdd8w0f")
+#@onready var inactive_stylebox: StyleBoxFlat = preload("uid://b1c8gk4cr5oa7")
+@onready var active_stylebox_dark: StyleBoxFlat = preload("uid://cetchnns5h8tx")
+@onready var inactive_stylebox_dark: StyleBoxFlat = preload("uid://cng68uuqfw6hb")
