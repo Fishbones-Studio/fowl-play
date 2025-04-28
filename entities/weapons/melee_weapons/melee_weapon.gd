f7c8a3fb25342c7efdf1e6b852b7e96289c4365c
Nrojt <tjornbrederoo@gmail.com>
2025-04-28 13:36:54 +0200

    fix: switched priorities for raycast and collisionshape

--- entities/weapons/melee_weapons/melee_weapon.gd
+++ entities/weapons/melee_weapons/melee_weapon.gd
@@ -25,13 +25,24 @@ func _check_for_hit() -> void:
 	if not hit_check_point or not weapon_hit_box:
 		printerr("MeleeWeapon: HitCheckPoint or WeaponHitBox node not found!")
 		return
+		
+	weapon_hit_box.collision_layer = 0
+	weapon_hit_box.collision_mask = hit_mask
 
+	# Collision shape check 
+	var overlapping_bodies := weapon_hit_box.get_overlapping_bodies()
+	for body in overlapping_bodies:
+		if body is Node3D and not hit_targets_this_swing.has(body):
+			hit_targets_this_swing.append(body)
+			print("Melee hit (area): ", body.name)
+			return
+
+	# Raycast check, fallback if no area hit
 	var space_state := get_world_3d().direct_space_state
 	if not space_state:
 		printerr("MeleeWeapon: Could not get DirectSpaceState!")
 		return
-
-	# Raycast check
+	
 	var ray_origin: Vector3 = hit_check_point.global_position
 	var ray_direction: Vector3 = -global_transform.basis.z.normalized()
 	var ray_end: Vector3 = ray_origin + ray_direction * ray_length
@@ -48,14 +59,5 @@ func _check_for_hit() -> void:
 		if collider is Node3D and not hit_targets_this_swing.has(collider):
 			hit_targets_this_swing.append(collider)
 			print("Melee hit (ray): ", collider.name)
-			return
 
-	weapon_hit_box.collision_layer = 0
-	weapon_hit_box.collision_mask = hit_mask
 	
-	# Collision shape check 
-	var overlapping_bodies := weapon_hit_box.get_overlapping_bodies()
-	for body in overlapping_bodies:
-		if body is Node3D and not hit_targets_this_swing.has(body):
-			hit_targets_this_swing.append(body)
-			print("Melee hit (area): ", body.name)
