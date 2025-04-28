a7b1731feaa122c42ec0febc67efd347588b588a
Nrojt <tjornbrederoo@gmail.com>
2025-04-28 12:30:22 +0200

    fix: remove unrequired signals

--- entities/weapons/melee_weapons/melee_weapon.gd
+++ entities/weapons/melee_weapons/melee_weapon.gd
@@ -5,20 +5,3 @@ extends Node3D
 @export var current_weapon: MeleeWeaponResource
 
 @onready var hit_area: Area3D = $HitArea
-
-
-func _ready() -> void:
-	# connect a signal to body entering the hit_area
-	hit_area.body_entered.connect(_on_hit_area_body_entered)
-	# connect a signal to body exiting the hit_area
-	hit_area.body_exited.connect(_on_hit_area_body_exited)
-
-
-func _on_hit_area_body_entered(body: Node) -> void:
-	if body is PhysicsBody3D:
-		SignalManager.weapon_hit_area_body_entered.emit(body)
-
-
-func _on_hit_area_body_exited(body: Node) -> void:
-	if body is PhysicsBody3D:
-		SignalManager.weapon_hit_area_body_exited.emit(body)
