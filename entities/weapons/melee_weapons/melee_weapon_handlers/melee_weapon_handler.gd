c1698585c517c14983dca122cce8f73786d367cb
Nrojt <tjornbrederoo@gmail.com>
2025-04-29 18:16:07 +0200

    removed another print

--- entities/weapons/melee_weapons/melee_weapon_handlers/melee_weapon_handler.gd
+++ entities/weapons/melee_weapons/melee_weapon_handlers/melee_weapon_handler.gd
@@ -21,7 +21,6 @@ extends Node3D
 @export_flags_3d_physics var weapon_collision_mask: int:
 	set(value):
 		weapon_collision_mask = value
-		print("Setting weapon mask to: %d" % value)
 		if current_weapon:
 			current_weapon.hit_mask = weapon_collision_mask
 		
