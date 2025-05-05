640ecf1a0e034f464b121e9559aab975f7e00b24
Nrojt <tjornbrederoo@gmail.com>
2025-05-05 12:44:05 +0200

    feat: progress bar

--- ui/poultry_man_menu/rebirth_shop/skill_tree_item.gd
+++ ui/poultry_man_menu/rebirth_shop/skill_tree_item.gd
@@ -3,23 +3,16 @@ extends VBoxContainer
 
 @export var upgrade_type: StatsEnums.UpgradeTypes
 @export var max_level: int = 5
-@onready var kind_indicator_label: Label = $KindIndicatorLabel
-@onready var buy_button: Button = $HBoxContainer/Buy_Button
-@onready var level_panels: Array = [
-	$HBoxContainer/Level1,
-	$HBoxContainer/Level2,
-	$HBoxContainer/Level3,
-	$HBoxContainer/Level4,
-	$HBoxContainer/Level5
-]
 
 signal shop_refresh_needed
 
 var current_level: int = 0
 var copied_stats: LivingEntityStats
-var purchased_color: Color = Color(0, 1, 0)
-var unpurchased_color: Color = Color(0.5, 0.5, 0.5)
 var upgrade_resources: Array[PermUpgradeResource] = []
+
+@onready var kind_indicator_label: Label = $KindIndicatorLabel
+@onready var buy_button: Button = $HBoxContainer/Buy_Button
+@onready var level_progress_bar: ProgressBar = $HBoxContainer/LevelProgressBar
 
 func _ready() -> void:
 	kind_indicator_label.text = StatsEnums.upgrade_type_to_string(upgrade_type)
@@ -27,34 +20,42 @@ func _ready() -> void:
 	current_level = upgrades.get(upgrade_type, 0)
 	if not copied_stats:
 		copied_stats = SaveManager.get_loaded_player_stats()
-	update_panels()
+	update_progress_bar()
 
-func init(_upgrade_type: StatsEnums.UpgradeTypes, _upgrades: Array[PermUpgradeResource], _copied_stats: LivingEntityStats) -> void:
+func init(
+	_upgrade_type: StatsEnums.UpgradeTypes,
+	_upgrades: Array[PermUpgradeResource],
+	_copied_stats: LivingEntityStats
+) -> void:
 	upgrade_type = _upgrade_type
 	upgrade_resources = _upgrades
 	copied_stats = _copied_stats
 	kind_indicator_label.text = StatsEnums.upgrade_type_to_string(_upgrade_type)
 	var upgrades_dict = SaveManager.get_loaded_player_upgrades()
 	current_level = upgrades_dict.get(upgrade_type, 0)
-	update_panels()
+	update_progress_bar()
 
 func _on_buy_button_pressed() -> void:
-	if !upgrade_resources : return
+	if !upgrade_resources:
+		return
 	if current_level < max_level and can_afford_upgrade():
 		purchase_upgrade()
 		current_level += 1
-		update_panels()
+		# --- Update progress bar instead of panels ---
+		update_progress_bar()
 		apply_upgrade()
 		save_upgrades()
 		emit_signal("shop_refresh_needed")
-		print("Damage: ",  copied_stats.attack)
+		print("Damage: ", copied_stats.attack)
 		print("Max Health: ", copied_stats.max_health)
 		print("Stamina: ", copied_stats.max_stamina)
-		print("Speed: " , copied_stats.speed)
+		print("Speed: ", copied_stats.speed)
 		print("Weight: ", copied_stats.weight)
 		print("Defense: ", copied_stats.defense)
 	else:
-		print("Cannot purchase upgrade. Either max level reached or not enough currency.")
+		print(
+			"Cannot purchase upgrade. Either max level reached or not enough currency."
+		)
 
 func can_afford_upgrade() -> bool:
 	if current_level >= upgrade_resources.size():
@@ -90,14 +91,15 @@ func apply_upgrade() -> void:
 		StatsEnums.UpgradeTypes.WEIGHT:
 			copied_stats.weight += upgrade_resource.weight_bonus
 
-func update_panels() -> void:
-	for i in range(level_panels.size()):
-		var panel = level_panels[i]
-		var color_rect = panel.get_node("ColorRect")
-		if i < current_level:
-			color_rect.color = purchased_color
-		else:
-			color_rect.color = unpurchased_color
+# Progress Bar
+func update_progress_bar() -> void:
+	if not level_progress_bar:
+		push_error("LevelProgressBar node not found!")
+		return
+	level_progress_bar.max_value = max_level
+	level_progress_bar.value = current_level
+	# Disable buy button if max level is reached
+	buy_button.disabled = current_level >= max_level
 
 func save_upgrades() -> void:
 	var upgrades = SaveManager.get_loaded_player_upgrades()
