93f624204bf7743473dab3b1d643016b8e07c195
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-05-05 11:39:31 +0200

    adjusted offset

--- ui/player_hud/sub_viewport_player.gd
+++ ui/player_hud/sub_viewport_player.gd
@@ -6,30 +6,30 @@ enum EntityTypes { Chicken, Boss }
 @export var camera: Camera3D
 @export var entity_types: EntityTypes
 
-var target: Node3D = null
+var entity: Node3D = null
 
 
 func _process(_delta: float) -> void:
 	match entity_types:
 		EntityTypes.Chicken:
 			if GameManager.chicken_player: 
-				target = GameManager.chicken_player
+				entity = GameManager.chicken_player
 		EntityTypes.Boss:
 			if GameManager.current_enemy:
-				target = GameManager.current_enemy
+				entity = GameManager.current_enemy
 		_:
 			push_error("Invalid entity type assigned to SubViewport")
 
-	if target: _update_camera()
+	if entity: _update_camera()
 
 
 func _update_camera() -> void:
 	# Position camera behind the target
-	var rotated_offset = target.transform.basis * offset
-	camera.global_position = target.global_position + rotated_offset
+	var rotated_offset = entity.transform.basis * offset
+	camera.global_position = entity.global_position + rotated_offset
 
 	# Calculate a target point in front of the entity
-	var target_position = target.global_position + target.transform.basis.z * 5.0
+	var target_position = entity.global_position + entity.transform.basis.z * 5.0
 	target_position.y = camera.global_position.y  # Match camera's height
 
 	# Look at the forward point
