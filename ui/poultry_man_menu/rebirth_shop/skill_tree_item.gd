14b1917f801bfbeb5a1a127a41f7e6aca4fb44c4
Nrojt <tjornbrederoo@gmail.com>
2025-05-05 13:05:46 +0200

    fix: showing currency

--- ui/poultry_man_menu/rebirth_shop/skill_tree_item.gd
+++ ui/poultry_man_menu/rebirth_shop/skill_tree_item.gd
@@ -9,11 +9,15 @@ signal shop_refresh_needed
 var current_level: int = 0
 var copied_stats: LivingEntityStats
 var upgrade_resources: Array[PermUpgradeResource] = []
+var prosperity_egg_icon: CompressedTexture2D = preload("uid://be0yl1q0uryjp")
+var feathers_of_rebirth_icon: CompressedTexture2D = preload("uid://brgdaqksfgmqu")
 
-@onready var kind_indicator_label: Label = $KindIndicatorLabel
-@onready var buy_button: Button = $HBoxContainer/Buy_Button
-@onready var level_progress_bar: ProgressBar = $HBoxContainer/LevelProgressBar
-@onready var level_label : Label = $HBoxContainer/LevelLabel
+@onready var kind_indicator_label: Label = %KindIndicatorLabel
+@onready var buy_button: Button = %BuyButton
+@onready var level_progress_bar: ProgressBar = %LevelProgressBar
+@onready var level_label: Label = %LevelLabel
+@onready var item_currency_icon: TextureRect = %ItemCurrencyIcon
+@onready var item_cost_label: Label = %ItemCostLabel
 
 func _ready() -> void:
 	kind_indicator_label.text = StatsEnums.upgrade_type_to_string(upgrade_type)
@@ -21,7 +25,7 @@ func _ready() -> void:
 	current_level = upgrades.get(upgrade_type, 0)
 	if not copied_stats:
 		copied_stats = SaveManager.get_loaded_player_stats()
-	update_progress_bar()
+	update_ui_elements()
 
 func init(
 	_upgrade_type: StatsEnums.UpgradeTypes,
@@ -34,16 +38,15 @@ func init(
 	kind_indicator_label.text = StatsEnums.upgrade_type_to_string(_upgrade_type)
 	var upgrades_dict = SaveManager.get_loaded_player_upgrades()
 	current_level = upgrades_dict.get(upgrade_type, 0)
-	update_progress_bar()
+	update_ui_elements()
 
 func _on_buy_button_pressed() -> void:
-	if !upgrade_resources:
+	if not upgrade_resources:
 		return
 	if current_level < max_level and can_afford_upgrade():
 		purchase_upgrade()
 		current_level += 1
-		# Update progress bar
-		update_progress_bar()
+		update_ui_elements()
 		apply_upgrade()
 		save_upgrades()
 		emit_signal("shop_refresh_needed")
@@ -54,19 +57,27 @@ func _on_buy_button_pressed() -> void:
 		print("Weight: ", copied_stats.weight)
 		print("Defense: ", copied_stats.defense)
 	else:
-		print(
-			"Cannot purchase upgrade. Either max level reached or not enough currency."
-		)
+		print("Cannot purchase upgrade. Either max level reached or not enough currency.")
 
 func can_afford_upgrade() -> bool:
 	if current_level >= upgrade_resources.size():
 		return false
 	var upgrade_resource = upgrade_resources[current_level]
-	return GameManager.feathers_of_rebirth >= upgrade_resource.cost
+	match upgrade_resource.currency_type:
+		CurrencyEnums.CurrencyTypes.FEATHERS_OF_REBIRTH:
+			return GameManager.feathers_of_rebirth >= upgrade_resource.cost
+		CurrencyEnums.CurrencyTypes.PROSPERITY_EGGS:
+			return GameManager.prosperity_eggs >= upgrade_resource.cost
+		_:
+			return false
 
 func purchase_upgrade() -> void:
 	var upgrade_resource = upgrade_resources[current_level]
-	GameManager.feathers_of_rebirth -= upgrade_resource.cost
+	match upgrade_resource.currency_type:
+		CurrencyEnums.CurrencyTypes.FEATHERS_OF_REBIRTH:
+			GameManager.feathers_of_rebirth -= upgrade_resource.cost
+		CurrencyEnums.CurrencyTypes.PROSPERITY_EGGS:
+			GameManager.prosperity_eggs -= upgrade_resource.cost
 
 func apply_upgrade() -> void:
 	if current_level == 0 or current_level > upgrade_resources.size():
@@ -92,17 +103,27 @@ func apply_upgrade() -> void:
 		StatsEnums.UpgradeTypes.WEIGHT:
 			copied_stats.weight += upgrade_resource.weight_bonus
 
-# Progress Bar
-func update_progress_bar() -> void:
-	if not level_progress_bar:
-		push_error("LevelProgressBar node not found!")
-		return
+func update_ui_elements() -> void:
 	level_progress_bar.max_value = max_level
 	level_progress_bar.value = current_level
-	# Disable buy button if max level is reached
 	buy_button.disabled = current_level >= max_level
-	
-	# update level label
+
+	if current_level < max_level and current_level < upgrade_resources.size():
+		var next_upgrade = upgrade_resources[current_level]
+		item_cost_label.text = str(next_upgrade.cost)
+		match next_upgrade.currency_type:
+			CurrencyEnums.CurrencyTypes.PROSPERITY_EGGS:
+				item_currency_icon.texture = prosperity_egg_icon
+			_:
+				item_currency_icon.texture = feathers_of_rebirth_icon
+		item_cost_label.show()
+		item_currency_icon.show()
+		buy_button.disabled = false
+	else:
+		item_cost_label.hide()
+		item_currency_icon.hide()
+		buy_button.disabled = true
+
 	if current_level < max_level:
 		level_label.text = str(current_level)
 	else:
