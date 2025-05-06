3c9aa43d7561d72fa9e2df5ba606b2d08e47e7b9
Nrojt <tjornbrederoo@gmail.com>
2025-05-06 17:05:23 +0200

    wip

--- entities/weapons/ranged_weapons/ranged_weapon_models/slingshot/projectile/slingshot_projectile.gd
+++ entities/weapons/ranged_weapons/ranged_weapon_models/slingshot/projectile/slingshot_projectile.gd
@@ -1,6 +1,5 @@
 extends RigidBody3D
 
-# After this amount of time, spawn the hazard, regardless of collisions
 @export var time_out: float = 2.0
 @export var hazard: PackedScene = preload("uid://cw81sd3kyuelj")
 
@@ -10,65 +9,56 @@ var firer_entity_stats: LivingEntityStats
 var _timer: float = 0.0
 var _has_spawned_hazard: bool = false
 
-
-func _ready() -> void:
-	# Connect the body_entered signal
-	body_entered.connect(_on_body_entered)
-
-# This function will be called by the weapon state
 func setup_projectile(p_firer_stats: LivingEntityStats, p_damage: int) -> void:
 	firer_entity_stats = p_firer_stats
 	base_damage = p_damage
-
-	# set the mask layer
-	if p_firer_stats.is_player:
-		collision_mask = 4
-	else:
-		collision_mask = 2
-
+	
+	# TODO: set up collision layer and fix on body entered
 
 func _physics_process(delta: float) -> void:
 	if _has_spawned_hazard:
-		return # Hazard already spawned, no need to do more
+		return
 
 	_timer += delta
-	if _timer >= time_out:
+	if _timer >= time_out and is_inside_tree():
 		_spawn_hazard()
-
 
 func _on_body_entered(body: Node) -> void:
 	if _has_spawned_hazard:
 		return
 
+	if not is_inside_tree():
+		return
+
 	print("Projectile collided with: ", body.name)
 	_spawn_hazard()
 
-
 func _spawn_hazard() -> void:
-	if _has_spawned_hazard: # Ensure it only spawns once
+	if _has_spawned_hazard:
 		return
 	_has_spawned_hazard = true
 
-	if not is_instance_valid(hazard):
-		push_error("Hazard PackedScene is not set for the projectile!")
-		queue_free() # Still clean up the projectile
+	if not is_inside_tree():
+		print("Not in tree, aborting hazard spawn.")
+		queue_free()
 		return
 
-	var hazard_instance : BaseHazard = hazard.instantiate()
+	if not is_instance_valid(hazard):
+		push_error("Hazard PackedScene is not set for the projectile!")
+		queue_free()
+		return
 
-	# Place the hazard at the projectile's current position
-	hazard_instance.global_position = self.global_position
+	var hazard_instance: BaseHazard = hazard.instantiate()
+	hazard_instance.global_position = global_position
 
-	# Add the hazard to the scene tree
-	var parent_node = get_parent()
+	var parent_node: Node = get_parent()
+	var scene_tree: SceneTree = get_tree()
+
+	queue_free()
+
 	if is_instance_valid(parent_node):
 		parent_node.add_child(hazard_instance)
 	else:
-		# Fallback to current scene if projectile has no parent
-		get_tree().current_scene.add_child(hazard_instance)
+		scene_tree.current_scene.add_child(hazard_instance)
 
-	# Pass along the damage info to the hazard
 	hazard_instance.damage = base_damage
-
- 	# Remove the projectile after spawning the hazard
-	queue_free()
