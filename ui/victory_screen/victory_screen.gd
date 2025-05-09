54e1550101f6c62ab553181a33fa840c09fe3645
NickFrietman <21055378@student.hhs.nl>
2025-05-09 16:16:35 +0200

    Victory screen is working. UI still needs to be done. Maybe a scetch of the artists ? Had no creativity to make this but the functionalty works

---
+++ ui/victory_screen/victory_screen.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var is_transitioning: bool = false
+
+@onready var label: Label = $Label
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+
+func _ready() -> void:
+	get_tree().paused = true
+	animation_player.play("victory")
+
+
+func _input(event: InputEvent) -> void:
+	if (((event is InputEventMouseButton
+		and event.button_index == MOUSE_BUTTON_LEFT) or event.is_action_pressed("ui_accept"))
+		and not animation_player.is_playing()
+		and not is_transitioning
+	):
+		_return_to_game_menu()
+		return
+
+	get_viewport().set_input_as_handled()
+
+
+func _return_to_game_menu() -> void:
+	is_transitioning = true
+	animation_player.play("RESET")
+
+	get_tree().paused = false
+	SignalManager.switch_game_scene.emit("uid://21r458rvciqo")
+	
+	UIManager.remove_ui(self)
