ceccb6faf4472d0c756141772bf41b9471a5bd74
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-05-16 01:17:17 +0200

    block input death state

--- entities/chicken_player/states/movement_states/death_state.gd
+++ entities/chicken_player/states/movement_states/death_state.gd
@@ -8,6 +8,8 @@ func enter(prev_state: BasePlayerMovementState, information: Dictionary = {}) ->
 
 	animation_tree.get("parameters/MovementStateMachine/playback").travel(self.name)
 
+	UIManager.game_input_blocked = true
+
 	if "initial_velocity" in information:
 		player.velocity = information["initial_velocity"]
 	else:
@@ -26,4 +28,5 @@ func physics_process(delta: float) -> void:
 # Using timer since for some reason AnimationNodeStateMachinePlayback never
 # stops playing, and it also transitions immediately when comparing anim length.
 func _on_timer_timeout() -> void:
+	UIManager.game_input_blocked = false
 	SignalManager.switch_ui_scene.emit(UIEnums.UI.DEATH_SCREEN)
