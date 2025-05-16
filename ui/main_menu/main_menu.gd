416932673e6f91a5b6340445917424891fc3b313
Nrojt <tjornbrederoo@gmail.com>
2025-05-16 13:17:28 +0200

    fix: controller support

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,14 +1,18 @@
 extends Control
 
+@onready var play_button: Button = $MarginContainer/PlayButton
+
 
 func _ready() -> void:
-	SettingsManager.load_settings( get_viewport(),get_window())
+	SettingsManager.load_settings(get_viewport(), get_window())
+
+	if play_button:
+		play_button.grab_focus()
 
 
 func _gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton or event is InputEventJoypadButton:
-		if event.pressed:
-			_on_play_button_pressed()
+	if (event is InputEventMouseButton and event.pressed) or event is InputEventKey:
+		_on_play_button_pressed()
 
 
 func _on_play_button_pressed() -> void:
@@ -22,8 +26,7 @@ func _on_settings_button_pressed() -> void:
 	if UIEnums.UI.SETTINGS_MENU in UIManager.ui_list:
 		UIManager.toggle_ui(UIEnums.UI.SETTINGS_MENU)
 	else:
-		SignalManager.add_ui_scene.emit(UIEnums.UI.SETTINGS_MENU) 
-
+		SignalManager.add_ui_scene.emit(UIEnums.UI.SETTINGS_MENU)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
