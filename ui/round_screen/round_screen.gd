d430d703828646d933084601cbde907e8e57114d
NickFrietman <94540681+NickFrietman@users.noreply.github.com>
2025-05-17 08:31:13 +0200

    Feature/victory screen (#223)
    * Victory screen is working. UI still needs to be done. Maybe a scetch of the artists ? Had no creativity to make this but the functionalty works
    
    * Victory screen is working. UI still needs to be done. Maybe a scetch of the artists ? Had no creativity to make this but the functionalty works
    
    * feat: added victory music
    fix: centered victory text
    
    * fix: actually center victory screen
    
    * wip
    
    * wip2
    
    * feat: currency amount gained on round screen
    
    * feat: currency overview for death screen
    
    * feat: music fixes
    
    * fix: update music
    
    * music
    
    * Update main_theme.tres
    
    ---------
    
    Co-authored-by: Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
    Co-authored-by: Nrojt <tjornbrederoo@gmail.com>
    Co-authored-by: cenkerA08 <158097944+cenkerA08@users.noreply.github.com>
    Co-authored-by: Tjorn <74906283+Nrojt@users.noreply.github.com>

--- ui/round_screen/round_screen.gd
+++ ui/round_screen/round_screen.gd
@@ -1,16 +1,21 @@
 extends Control
 
-var display_text: String 
+var display_text: String
+var currency_overview_change : CurrencyOverviewDict
 
-@onready var title: Label = $CenterContainer/Label
+@onready var title: Label = $VboxContainer/TitleLabel
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_timer: Timer = $AnimationTimer
+@onready var currency_overview : CurrencyOverview = %CurrencyOverview
 
 
 func _ready() -> void:
 	title.text = display_text
 	animation_player.play("fade")
 	animation_timer.start()
+	print(currency_overview_change.currency_dict)
+	currency_overview.label_amount_dictionary = currency_overview_change
+	currency_overview.update_label_container()
 
 
 func _input(_event: InputEvent) -> void:
@@ -19,6 +24,7 @@ func _input(_event: InputEvent) -> void:
 
 func setup(params: Dictionary) -> void:
 	display_text = params.get("display_text")
+	currency_overview_change = params.get("currency_dict", CurrencyOverviewDict.new({}))
 
 
 func _on_animation_timer_timeout() -> void:
