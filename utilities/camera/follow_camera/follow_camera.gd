cb8f0b77e82f4ba008af507f73cd2eeb2fe7f1d6
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-05-19 20:45:52 +0200

    Fix/settings-graphics+controls (#285)
    * Update settings_manager.gd
    
    * Fix control settings not loading correctly (stoopid)
    
    * Update follow_camera.gd
    
    ---------
    
    Co-authored-by: Junyi Xie <57712951+junyi-xie@users.noreply.github.com>

--- utilities/camera/follow_camera/follow_camera.gd
+++ utilities/camera/follow_camera/follow_camera.gd
@@ -131,12 +131,13 @@ func _load_camera_settings() -> void:
 		for control_setting in config.get_section_keys(cfg_name):
 			camera_settings[control_setting] = config.get_value(cfg_name, control_setting)
 
-	match camera_settings.keys():
-		"horizontal_sensitivity": horizontal_sensitivity = camera_settings["horizontal_sensitivity"]["value"]
-		"vertical_sensitivity": vertical_sensitivity = camera_settings["vertical_sensitivity"]["value"]
-		"controller_sensitivity": controller_sensitivity = camera_settings["controller_sensitivity"]["value"]
-		"controller_sensitivity": controller_sensitivity = camera_settings["controller_sensitivity"]["value"]
-		"camera_up_tilt": max_degrees = camera_settings["camera_up_tilt"]["value"]
-		"camera_down_tilt": min_degrees = camera_settings["camera_down_tilt"]["value"]
-		"invert_x_axis": invert_x_axis = camera_settings["invert_x_axis"]["value"]
-		"invert_y_axis": invert_y_axis = camera_settings["invert_y_axis"]["value"]
+	for key in camera_settings.keys():
+		match key:
+			"horizontal_sensitivity": horizontal_sensitivity = camera_settings["horizontal_sensitivity"]["value"]
+			"vertical_sensitivity": vertical_sensitivity = camera_settings["vertical_sensitivity"]["value"]
+			"controller_sensitivity": controller_sensitivity = camera_settings["controller_sensitivity"]["value"]
+			"controller_sensitivity": controller_sensitivity = camera_settings["controller_sensitivity"]["value"]
+			"camera_up_tilt": max_degrees = camera_settings["camera_up_tilt"]["value"]
+			"camera_down_tilt": min_degrees = camera_settings["camera_down_tilt"]["value"]
+			"invert_x_axis": invert_x_axis = camera_settings["invert_x_axis"]["value"]
+			"invert_y_axis": invert_y_axis = camera_settings["invert_y_axis"]["value"]
