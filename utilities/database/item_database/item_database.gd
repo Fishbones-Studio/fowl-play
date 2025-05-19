740d3fb89b9ba0dc7ae5ffa81ab1ff2a382e286f
Nrojt <tjornbrederoo@gmail.com>
2025-05-19 12:40:11 +0200

    hotfix: refactor resource loading in item and perm upgrade databases

--- utilities/database/item_database/item_database.gd
+++ utilities/database/item_database/item_database.gd
@@ -1,6 +1,5 @@
 extends BaseDatabase
 
-
 static func _load_resources() -> Array[BaseResource]:
 	var temp_items: Array[BaseResource] = []
 	load_scene_resources("res://entities/weapons/melee_weapons/melee_weapon_models", "current_weapon", temp_items)
@@ -8,46 +7,38 @@ static func _load_resources() -> Array[BaseResource]:
 	load_scene_resources("res://entities/abilities/ability_models/", "", temp_items)
 	return temp_items
 
-
 static func load_scene_resources(
 	path: String,
 	resource_property: String,
 	temp_items: Array[BaseResource]
 ) -> void:
-	var dir := DirAccess.open(path)
-	if !dir:
-		push_error("Can't open directory: ", path)
-		return
-
-	for subdir in dir.get_directories():
-		var subdir_path := path.path_join(subdir)
-		var sub_dir := DirAccess.open(subdir_path)
-		if !sub_dir:
+	var subdirs: PackedStringArray = ResourceLoader.list_directory(path)
+	for subdir in subdirs:
+		if not subdir.ends_with("/"):
 			continue
-
+		var subdir_path := path.path_join(subdir.trim_suffix("/"))
+		var files: PackedStringArray = ResourceLoader.list_directory(subdir_path)
 		var tres_file: String = ""
 		var tscn_file: String = ""
 
-		# First try to load .tres file
-		for file in sub_dir.get_files():
-			if file.ends_with(".remap"):
-				file = file.substr(0, file.length() - 6)  # Remove .remap
-			if file.ends_with(".tres"):
+		for file in files:
+			if file.begins_with(".") or file.ends_with("/"):
+				continue
+			if file.ends_with(".tres") and tres_file == "":
 				tres_file = file
-				break  # Prefer .tres, stop searching
-			elif file.ends_with(".tscn"):
-				if tscn_file == "":
-					tscn_file = file  # Use the first .tscn found
+			elif file.ends_with(".tscn") and tscn_file == "":
+				tscn_file = file
 
 		if tres_file != "":
-			var resource: Resource = load(subdir_path.path_join(tres_file))
+			var resource: Resource = ResourceLoader.load(subdir_path.path_join(tres_file))
 			if resource is BaseResource:
 				temp_items.append(resource)
-		elif tscn_file != "":
-			var scene := load(subdir_path.path_join(tscn_file)) as PackedScene
-			var instance: Node = scene.instantiate()
-			if instance.has_method("get"):
-				var hidden_resource = instance.get(resource_property)
-				if hidden_resource is BaseResource:
-					temp_items.append(hidden_resource)
-			instance.queue_free()
+		elif tscn_file != "" and resource_property != "":
+			var scene := ResourceLoader.load(subdir_path.path_join(tscn_file)) as PackedScene
+			if scene:
+				var instance: Node = scene.instantiate()
+				if instance and instance.has_method("get"):
+					var hidden_resource = instance.get(resource_property)
+					if hidden_resource is BaseResource:
+						temp_items.append(hidden_resource)
+				instance.queue_free()
