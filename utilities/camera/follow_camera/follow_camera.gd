2672c8fb0e78595b99abf1f5f0ef27de0c503a21
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-05-19 20:39:06 +0200

    Fix control settings not loading correctly (stoopid)

--- utilities/camera/follow_camera/follow_camera.gd
+++ utilities/camera/follow_camera/follow_camera.gd
@@ -130,13 +130,15 @@ func _load_camera_settings() -> void:
 	if config.load(cfg_path) == OK and config.has_section(cfg_name):
 		for control_setting in config.get_section_keys(cfg_name):
 			camera_settings[control_setting] = config.get_value(cfg_name, control_setting)
+		print(camera_settings.keys())
 
-	match camera_settings.keys():
-		"horizontal_sensitivity": horizontal_sensitivity = camera_settings["horizontal_sensitivity"]["value"]
-		"vertical_sensitivity": vertical_sensitivity = camera_settings["vertical_sensitivity"]["value"]
-		"controller_sensitivity": controller_sensitivity = camera_settings["controller_sensitivity"]["value"]
-		"controller_sensitivity": controller_sensitivity = camera_settings["controller_sensitivity"]["value"]
-		"camera_up_tilt": max_degrees = camera_settings["camera_up_tilt"]["value"]
-		"camera_down_tilt": min_degrees = camera_settings["camera_down_tilt"]["value"]
-		"invert_x_axis": invert_x_axis = camera_settings["invert_x_axis"]["value"]
-		"invert_y_axis": invert_y_axis = camera_settings["invert_y_axis"]["value"]
+	for key in camera_settings.keys():
+		match key:
+			"horizontal_sensitivity": horizontal_sensitivity = camera_settings["horizontal_sensitivity"]["value"]
+			"vertical_sensitivity": vertical_sensitivity = camera_settings["vertical_sensitivity"]["value"]
+			"controller_sensitivity": controller_sensitivity = camera_settings["controller_sensitivity"]["value"]
+			"controller_sensitivity": controller_sensitivity = camera_settings["controller_sensitivity"]["value"]
+			"camera_up_tilt": max_degrees = camera_settings["camera_up_tilt"]["value"]
+			"camera_down_tilt": min_degrees = camera_settings["camera_down_tilt"]["value"]
+			"invert_x_axis": invert_x_axis = camera_settings["invert_x_axis"]["value"]
+			"invert_y_axis": invert_y_axis = camera_settings["invert_y_axis"]["value"]
