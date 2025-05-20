bd2c3403fac14578630719b9f250eef2f0a5fc46
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-05-20 15:57:57 +0200

    Update state_label.gd

--- ui/player_debug_menu/state_label.gd
+++ ui/player_debug_menu/state_label.gd
@@ -1,10 +1,9 @@
 extends Label
 
-func _ready():
+
+func _ready() -> void:
 	SignalManager.player_transition_state.connect(_on_player_state_transition)
-	pass
 
 
-func _on_player_state_transition(next_state: PlayerEnums.PlayerStates, information: Dictionary):
-	# TODO: when spamming buttons, this ui stays stuck displaying the previous state
+func _on_player_state_transition(next_state: PlayerEnums.PlayerStates, information: Dictionary) -> void:
 	text = "State: %s \nAdditional information: %s" % [[PlayerEnums.PlayerStates.keys()[next_state]], str(information)]
