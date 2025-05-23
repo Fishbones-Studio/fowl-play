e9af4dd9a309bad34d41f4c3a7192e88244dde48
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-05-24 01:48:16 +0200

    hold

--- ui/player_hud/item_slot/ui_item_controller_icon.gd
+++ ui/player_hud/item_slot/ui_item_controller_icon.gd
@@ -22,15 +22,18 @@ extends Control
 @onready var original_x_position: float = controller_icon.position.x
 @onready var weapon_switch_icon: Sprite2D = %weapon_switch_icon
 
+
 func _ready() -> void:
 	SignalManager.keybind_changed.connect(_on_keybind_changed)
 	_update_controller_icon()
 	_update_hold_label()
 
+
 func _on_keybind_changed(action_name: String) -> void:
 	if action_name == "*" or action_name.to_lower() == input_action.to_lower():
 		IconManager.clear_cache(input_action)
 		_update_controller_icon()
+
 
 func _update_controller_icon() -> void:
 	if not is_inside_tree() or not controller_icon or input_action.is_empty():
@@ -40,14 +43,16 @@ func _update_controller_icon() -> void:
 	controller_icon.texture = tex
 	controller_icon.visible = tex != null
 
+
 func _update_hold_label() -> void:
 	if not is_inside_tree() or not controller_icon:
 		return
 
 	if has_node("%HoldLabel"):
 		%HoldLabel.visible = show_hold_label
-	
+
 	controller_icon.position.x = original_x_position + 10 if show_hold_label else original_x_position
+
 
 func _update_switch_icon() -> void:
 	if not is_inside_tree() or not controller_icon:
@@ -55,4 +60,3 @@ func _update_switch_icon() -> void:
 
 	if has_node("%weapon_switch_icon"):
 		weapon_switch_icon.visible = show_switch_icon
-	
