59ea9c042fbe45a3a985e25125733e4f66ece748
Nrojt <tjornbrederoo@gmail.com>
2025-05-26 10:54:57 +0200

    feat: rotating labels

--- ui/3d_label/custom_layer_label_3d.gd
+++ ui/3d_label/custom_layer_label_3d.gd
@@ -2,6 +2,18 @@ class_name CustomLayerLabel3D
 extends Label3D
 
 @export_range(1, 20) var visual_layer: int = 3
+## When true, makes the label rotate towards the player
+@export var label_lookat_player := false :
+	set(value):
+		label_lookat_player = value
+		set_process(value)
 
 func _ready() -> void:
 	layers = 1 << (visual_layer - 1)
+
+func _process(delta: float) -> void:
+	if GameManager.chicken_player:
+		# Make the Label3D look at the player's global position.
+		look_at(GameManager.chicken_player.global_position, Vector3.UP)
+		# Rotate the label 180 degrees around its local Y-axis to face correctly.
+		rotate_object_local(Vector3.UP, PI)
