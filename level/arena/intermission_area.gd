64aa23efd955c6495039d4a2f28cdbf5412ebdeb
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-05-26 18:01:38 +0200

    Update intermission_area.gd

--- level/arena/intermission_area.gd
+++ level/arena/intermission_area.gd
@@ -25,6 +25,9 @@ func _physics_process(_delta: float) -> void:
 
 
 func _on_next_enemy_selected(next_enemy: Enemy) -> void:
+	if next_enemy.model and next_enemy.model.get_child_count():
+		enemy_model = next_enemy.model.get_child(0).duplicate()
+		assert(enemy_model, "No enemy model")
 		next_enemy_box.add_child(enemy_model)
 		next_enemy_box.dialogue_folder_path = next_enemy.dialogue_path
 		initial_enemy_rotation = enemy_model.basis.orthonormalized()
