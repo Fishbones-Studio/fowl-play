e37c4568c25caf97471045270e39e23b098a54ee
Nrojt <tjornbrederoo@gmail.com>
2025-05-26 14:46:08 +0200

    feat: flyers

--- utilities/enums/scene_enums.gd
+++ utilities/enums/scene_enums.gd
@@ -12,3 +12,6 @@ const PATHS :  Dictionary[Scenes, String] = {
 	Scenes.SEWER_LEVEL : "uid://bhnqi4fnso1hh",
 	Scenes.POULTRY_MAN_MENU : "uid://21r458rvciqo"
 }
+
+static func scene_to_string(scene: Scenes) -> String:
+	return Scenes.keys()[scene].capitalize()
