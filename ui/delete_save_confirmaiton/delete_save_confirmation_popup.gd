0b9d9495455e6dc6894ca9782eeeb870c736c387
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-05-27 21:56:25 +0200

    Update delete_save_confirmation_popup.gd

--- ui/delete_save_confirmaiton/delete_save_confirmation_popup.gd
+++ ui/delete_save_confirmaiton/delete_save_confirmation_popup.gd
@@ -4,6 +4,7 @@ extends ConfirmationScreen
 func _ready() -> void:
 	cancel_button.grab_focus()
 	title.text = "Delete save file"
+	description.text = "This will [color=yellow]permanently delete[/color] your save file, including all your progress, items, equipment, unlocked abilities, and any other data associated with your game. This action cannot be undone."
 
 
 func on_confirm_button_pressed() -> void:
