904fcb89903c081d02c32fb10d5373e83d385767
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-05-31 13:36:17 +0200

    Adjusted feather model and fixed textures (#336)
    * added new feather texture
    
    * Update feather.png
    
    * Update attack_state.gd
    
    * update mesh of launch effect
    
    * update particle launch alignement
    
    * better lighting
    
    * Update feather.tres

--- entities/weapons/ranged_weapons/ranged_weapon_models/feather/states/attack_state.gd
+++ entities/weapons/ranged_weapons/ranged_weapon_models/feather/states/attack_state.gd
@@ -15,6 +15,7 @@ var _attack_elapsed: float = 0.0
 @onready var wind_effect : GPUParticles3D = $"../../WindEffect"
 @onready var launch_effect : GPUParticles3D = $"../../LaunchEffect"
 
+
 func enter(_previous_state, _info: Dictionary = {}) -> void:
 	if weapon.entity_stats.is_player:
 		SignalManager.cooldown_item_slot.emit(
@@ -30,6 +31,7 @@ func enter(_previous_state, _info: Dictionary = {}) -> void:
 	_wind_attack_elapsed = 0.0
 	launch_effect.emitting = true
 
+
 func physics_process(delta: float) -> void:
 	if is_instance_valid(_pending_raycast):
 		if _pending_raycast.is_colliding():
@@ -39,13 +41,14 @@ func physics_process(delta: float) -> void:
 	if feather_model.visible:
 		# Update the feather model position based on the attack duration
 		_attack_elapsed += delta
-		var t = clamp(_attack_elapsed / weapon.current_weapon.attack_duration, 0.0, 1.0)
-		feather_model.global_position = _feather_start_pos.lerp(_feather_end_pos, t)
+		var weight: float = clamp(_attack_elapsed / weapon.current_weapon.attack_duration, 0.0, 1.0)
+		feather_model.global_position = _feather_start_pos.lerp(_feather_end_pos, weight)
 
 		# Wind effect lags behind the feather
 		_wind_attack_elapsed += delta
-		var wind_t = clamp((_wind_attack_elapsed - wind_lag_time) / weapon.current_weapon.attack_duration, 0.0, 1.0)
-		wind_effect.global_position = _feather_start_pos.lerp(_feather_end_pos, wind_t)
+		var wind_weight: float = clamp((_wind_attack_elapsed - wind_lag_time) / weapon.current_weapon.attack_duration, 0.0, 1.0)
+		wind_effect.global_position = _feather_start_pos.lerp(_feather_end_pos, wind_weight)
+
 
 func exit() -> void:
 	_pending_raycast = null
@@ -57,13 +60,15 @@ func exit() -> void:
 	feather_model.position = Vector3.ZERO
 	wind_effect.position = Vector3(0, 0, -0.6)
 
+
 func _fire_single_shot() -> void:
 	var fire_direction: Vector3 = -attack_origin.global_basis.z.normalized()
 	var max_range: float = weapon.current_weapon.max_range
 	_feather_start_pos = attack_origin.global_position
 	_feather_end_pos = _feather_start_pos + fire_direction * max_range
 	_pending_raycast = _create_raycast(_feather_start_pos, fire_direction, max_range)
-	
+
+
 func _create_raycast(
 	origin: Vector3,
 	direction: Vector3,
@@ -90,7 +95,8 @@ func _create_raycast(
 
 	return raycast
 
-func _start_visualization_timer():
+
+func _start_visualization_timer() -> void:
 	if _visualization_timer:
 		_visualization_timer.queue_free()
 	_visualization_timer = Timer.new()
@@ -100,7 +106,8 @@ func _start_visualization_timer():
 	_visualization_timer.timeout.connect(_on_visualization_timer_timeout)
 	_visualization_timer.start()
 
-func _on_visualization_timer_timeout():
+
+func _on_visualization_timer_timeout() -> void:
 	transition_signal.emit(WeaponEnums.WeaponState.COOLDOWN, {})
 	if _visualization_timer:
 		_visualization_timer.queue_free()
