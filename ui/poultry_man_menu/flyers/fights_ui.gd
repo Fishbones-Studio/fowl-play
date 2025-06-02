46bc9d4050c4986ce661400a976b7c8718e4602a
Nrojt <tjornbrederoo@gmail.com>
2025-06-02 14:35:39 +0200

    fix: loading all poultry man ui during initial load now

--- ui/poultry_man_menu/flyers/fights_ui.gd
+++ ui/poultry_man_menu/flyers/fights_ui.gd
@@ -5,18 +5,15 @@ extends Control
 @onready var fights_container: GridContainer = %FightsContainer
 @onready var arena_flyer_resource: PackedScene = preload("uid://b68pl3dx4qrx7")
 
-
 func _ready() -> void:
-	_add_arena_flyers()
-	_setup_controller_navigation()
-
+	_add_arena_flyers.call_deferred()
+	_setup_controller_navigation.call_deferred()
 
 func _add_arena_flyers() -> void:
 	for flyer_resource in flyers_to_setup:
 		var flyer: PanelContainer = arena_flyer_resource.instantiate()
 		fights_container.add_child(flyer)
 		flyer.setup(flyer_resource)
-
 
 func _setup_controller_navigation() -> void:
 	await get_tree().process_frame
@@ -26,11 +23,9 @@ func _setup_controller_navigation() -> void:
 		if child is PanelContainer:
 			child.focus_mode = Control.FOCUS_ALL
 			focusable_flyers.append(child)
-			print(child)
 
 	if focusable_flyers.size() > 0:
 		focusable_flyers[0].grab_focus()
-
 
 func _on_close_button_pressed() -> void:
 	UIManager.toggle_ui(UIEnums.UI.POULTRYMAN_FIGHT_FLYERS)
--- ui/poultry_man_menu/flyers/flyer/arena_flyer.gd
+++ ui/poultry_man_menu/flyers/flyer/arena_flyer.gd
@@ -1,35 +1,35 @@
 extends PanelContainer
 
-@export var scene_to_load : SceneEnums.Scenes
+@export var scene_to_load: SceneEnums.Scenes
 
 var normal_stylebox: StyleBoxFlat = preload("uid://ceyysiao8q2tl")
 var hover_stylebox: StyleBoxFlat = preload("uid://c80bewaohqml0")
-var flyer_resource : ArenaFlyerResource
+var flyer_resource: ArenaFlyerResource
 
-@onready var arena_label : Label = %ArenaLabel
-@onready var arena_icon : TextureRect = %ArenaIcon
-
+@onready var arena_label: Label = %ArenaLabel
+@onready var arena_icon: TextureRect = %ArenaIcon
 
 func _ready() -> void:
 	focus_mode = Control.FOCUS_ALL
-	if not theme:
-		theme = Theme.new()
-	theme.set_stylebox("panel", "PanelContainer", normal_stylebox)
+	add_theme_stylebox_override("panel", normal_stylebox)
 
-
-func setup(_flyer_resource : ArenaFlyerResource):
+func setup(_flyer_resource: ArenaFlyerResource) -> void:
 	flyer_resource = _flyer_resource
-	
 	scene_to_load = flyer_resource.scene_to_load
-	arena_icon.texture = flyer_resource.arena_icon
-	
+	arena_icon.texture = flyer_resource.arena_icon  # Now arena_icon is not null
 	arena_label.text = flyer_resource.title
-
 
 func _trigger_scene_load() -> void:
 	await get_tree().process_frame
-	UIManager.load_game_with_loading_screen(scene_to_load, UIEnums.UI.PLAYER_HUD, {}, {"enemies" : flyer_resource.get_combined_enemy_scenes(), "max_rounds" : flyer_resource.rounds})
-
+	UIManager.load_game_with_loading_screen(
+		scene_to_load,
+		UIEnums.UI.PLAYER_HUD,
+		{},
+		{
+			"enemies": flyer_resource.get_combined_enemy_scenes(),
+			"max_rounds": flyer_resource.rounds
+		}
+	)
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -38,22 +38,14 @@ func _on_gui_input(event: InputEvent) -> void:
 	elif event.is_action_pressed("ui_accept") and has_focus():
 		_trigger_scene_load()
 
-
 func _on_focus_entered() -> void:
-	if not theme:
-		theme = Theme.new()
-	theme.set_stylebox("panel", "PanelContainer", hover_stylebox)
-
+	add_theme_stylebox_override("panel", hover_stylebox)
 
 func _on_focus_exited() -> void:
-	if not theme:
-		theme = Theme.new()
-	theme.set_stylebox("panel", "PanelContainer", normal_stylebox)
-
+	add_theme_stylebox_override("panel", normal_stylebox)
 
 func _on_mouse_entered() -> void:
 	grab_focus()
-
 
 func _on_mouse_exited() -> void:
 	_on_focus_exited()
