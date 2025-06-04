a6e3e15c2a51b176467198b13e2b0eb5ea39c76b
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-06-04 22:36:15 +0200

    update ability description and changed color to sun ish

--- entities/abilities/ability_models/sanctuary_of_firebloom/sanctuary_of_firebloom.gd
+++ entities/abilities/ability_models/sanctuary_of_firebloom/sanctuary_of_firebloom.gd
@@ -9,8 +9,7 @@ var damage: float:
 		var stats: LivingEntityStats = ability_holder.stats
 		return pow(max_blasts, 1.5) * ((1.0 + (stats.attack / 100)))
 
-var target: Node3D = null
-
+var _target: Node3D = null
 var _hit_bodies: Array = []
 var _blast_count: int = 0
 var _current_damage: float = 0.0
@@ -19,6 +18,7 @@ var _current_damage: float = 0.0
 @onready var detection_area: Area3D = $DetectionArea
 @onready var hit_area: Area3D = $HitArea
 @onready var gpu_particles: GPUParticles3D = %GPUParticles3D
+@onready var camera: FollowCamera = get_tree().get_first_node_in_group("FollowCamera")
 
 
 func _ready() -> void:
@@ -30,8 +30,8 @@ func _ready() -> void:
 
 
 func activate() -> void:
-	target = _get_closest_target(detection_area)
-	if target == null:
+	_target = _get_closest_target(detection_area)
+	if _target == null:
 		print("Cannot perform %s with no target in range." % name)
 		return
 
@@ -51,8 +51,8 @@ func activate() -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	if target and not blast_timer.is_stopped(): 
-		global_position = target.global_position
+	if _target and not blast_timer.is_stopped():
+		global_position = _target.global_position
 
 
 func _get_closest_target(area: Area3D) -> Node3D:
@@ -90,6 +90,9 @@ func _on_blast_timer_timeout() -> void:
 			)
 
 	gpu_particles.emitting = true
+
+	if camera:
+		camera.apply_shake(1.0 + (_blast_count * 0.1))
 
 	_blast_count += 1
 	if _blast_count < max_blasts:
