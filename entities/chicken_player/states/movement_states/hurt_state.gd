8fc7f93affa022a5f2e602e63c432b0fb28a019b
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-06-04 21:22:09 +0200

    add stun to chicken player

--- entities/chicken_player/states/movement_states/hurt_state.gd
+++ entities/chicken_player/states/movement_states/hurt_state.gd
@@ -4,6 +4,7 @@ extends BasePlayerMovementState
 
 var _knockback: Vector3
 var _is_immobile: bool
+var _is_stunned: bool
 
 @onready var immobile_timer: Timer = $ImmobileTimer
 @onready var on_hurt: AudioStreamPlayer = $HurtSFX
@@ -24,15 +25,28 @@ func enter(prev_state: BasePlayerMovementState, info: Dictionary = {}) -> void:
 	player.velocity.x = 0
 	player.velocity.z = 0
 
+	var immobile_time: float = 0.0
 	if "knockback" in info:
 		_knockback = info["knockback"]
 
+		if not player.velocity.is_zero_approx():
+			_knockback *= 2
+
 		# Set immobile time
-		var immobile_time: float = _knockback.length() / knockback_decay
-		if not is_equal_approx(immobile_time, 0):
-			immobile_timer.wait_time = immobile_time
-			immobile_timer.start()
-			_is_immobile = true
+		immobile_time = _knockback.length() / knockback_decay
+
+	if "stun_time" in info:
+		var stun_time: float = info["stun_time"]
+
+		if stun_time > immobile_time:
+			immobile_time = stun_time
+			_is_stunned = true
+
+	if not is_zero_approx(immobile_time):
+		immobile_timer.wait_time = immobile_time
+		immobile_timer.start()
+		_is_immobile = true
+		immobile_time = 0.0
 
 
 func physics_process(delta: float) -> void:
@@ -42,7 +56,11 @@ func physics_process(delta: float) -> void:
 		player.velocity += _knockback
 		_knockback = _knockback.move_toward(Vector3.ZERO, knockback_decay * delta)
 
-	if not _is_immobile:
+	if _is_stunned and _knockback.is_zero_approx():
+		player.velocity.x = 0.0
+		player.velocity.z = 0.0
+
+	if not _is_immobile and not _is_stunned:
 		if not player.is_on_floor():
 			SignalManager.player_transition_state.emit(PlayerEnums.PlayerStates.FALL_STATE, {
 				"initial_velocity": player.velocity,
@@ -64,3 +82,4 @@ func physics_process(delta: float) -> void:
 
 func _on_immobile_timer_timeout() -> void:
 	_is_immobile = false
+	_is_stunned = false
