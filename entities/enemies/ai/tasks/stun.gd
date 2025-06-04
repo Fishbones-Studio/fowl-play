c2f7f4b6900bc75650fa7ccef58c62058b1629e6
Nrojt <tjornbrederoo@gmail.com>
2025-06-04 17:00:33 +0200

    fix: update behavior tree for Bob to include stun actions

---
+++ entities/enemies/ai/tasks/stun.gd
@@ -0,0 +1,24 @@
+@tool
+extends BTAction
+
+## The amount of time to stun the enemy for
+@export var stun_time : float = 1.0
+
+var _timer : float = 0.0
+
+func _generate_name() -> String:
+	return "Stun ➜ Agent"
+
+func _enter() -> void:
+	agent.is_stunned = true
+	_timer = 0.0
+
+func _exit() -> void:
+	agent.is_stunned = false
+
+func _tick(_delta: float) -> Status:
+	_timer += _delta
+	if _timer < stun_time:
+		return Status.RUNNING
+	else:
+		return Status.SUCCESS
