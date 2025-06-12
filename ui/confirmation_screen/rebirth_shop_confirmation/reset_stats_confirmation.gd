cb59db13633408c1df7a00838655032125c85d5a
Junyi Xie <57712951+junyi-xie@users.noreply.github.com>
2025-06-13 00:37:19 +0200

    Update reset_stats_confirmation.gd

--- ui/confirmation_screen/rebirth_shop_confirmation/reset_stats_confirmation.gd
+++ ui/confirmation_screen/rebirth_shop_confirmation/reset_stats_confirmation.gd
@@ -6,7 +6,7 @@ var stats_reset_signal: Signal
 func _ready() -> void:
 	cancel_button.grab_focus()
 	title.text = "Reset stats"
-	description.text = "Are you sure you want to reset your stats? This will [color=yellow]reset your current stats to their default values[/color] and refund [color=yellow]80%[/color] of the funds you’ve spent."
+	description.text = "Are you sure you want to reset your stats? This will [color=yellow]reset your current stats to their default values[/color] and refund [color=yellow]80%[/color] of the currency you’ve spent."
 
 
 func _input(_event: InputEvent) -> void:
