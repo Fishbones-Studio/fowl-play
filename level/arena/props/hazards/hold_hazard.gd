5371cc23fb243c7e4f9d0929093b9b01963df8c2
CenkerA08 <cenker08@live.nl>
2025-06-19 16:45:04 +0200

    added more sounds for , ground saw, peck, knife, dagger, sea of flames, spike trap

--- level/arena/props/hazards/hold_hazard.gd
+++ level/arena/props/hazards/hold_hazard.gd
@@ -6,13 +6,16 @@ extends BaseHazard
 @export var damage_interval: float = 2.0  ## Time between damage ticks
 @export var animation_name: StringName
 @export var animation_delay: float = 2.5
+@export var sound_fx: AudioStreamPlayer3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 
 func _ready() -> void:
 	await get_tree().create_timer(randf_range(0.0, animation_delay)).timeout
 	animation_player.play(animation_name)
+
 
 
 func _process(delta: float) -> void:
