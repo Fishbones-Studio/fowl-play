1be2128f09a3558a99a6f154c835e63c88c3ed72
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-06-26 23:51:04 +0200

    Increase controller sensitivity (#483)
    Co-authored-by: Junyi Xie <57712951+junyi-xie@users.noreply.github.com>

--- ui/settings_menu/controls/controls_settings.gd
+++ ui/settings_menu/controls/controls_settings.gd
@@ -5,7 +5,7 @@ extends Resource
 @export var default_settings: Array[Dictionary] = [
 	{"horizontal_sensitivity": {"type": "slider", "min": 0.1, "max": 2.0, "step": 0.1, "value": 0.5}},
 	{"vertical_sensivity": {"type": "slider", "min": 0.1, "max": 2.0, "step": 0.1, "value": 0.5}},
-	{"controller_sensivity": {"type": "slider", "min": 0.1, "max": 8.0, "step": 0.1, "value": 8.0}},
+	{"controller_sensivity": {"type": "slider", "min": 0.1, "max": 16.0, "step": 0.1, "value": 8.0}},
 	{"camera_up_tilt": {"type": "slider", "min": 0, "max": 90, "step": 1, "value": 45}},
 	{"camera_down_tilt": {"type": "slider", "min": -90, "max": 0, "step": 1, "value": -90}},
 	{"camera_spring_length": {"type": "slider", "min": 2.5, "max": 6.0, "step": 0.1, "value": 3.0}},
